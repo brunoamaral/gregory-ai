ddb84c46b58012185f809e18b6419c71cfa6988d
Bruno Amaral <mail@brunoamaral.eu>
2021-05-28 19:09:30 +0100

    generate files from json

--- build.py
+++ build.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import os
 import requests
-import shutil
 import subprocess
+import json 
+import pathlib
+from pathlib import Path
+
 # set variables
 path = "/home/gregory/gregory"
 server = "HTTPS://johnny.1q83.me/"
@@ -10,7 +13,6 @@ website_path = "/var/www/labs.brunoamaral.eu/"
 # Workflow starts
 os.chdir(path)
 ## Optional
-import git
 g = git.cmd.Git(path)
 g.pull()
 # Get articles
@@ -22,15 +24,18 @@ with open(file_name, "w") as f:
 file_name = path + '/content/api/articles.json'
 with open(file_name, "w") as f:
     f.write(res.text)
+    f.close()
 # Get trials
 url = server + 'api/trials/all'
 res = requests.get(url)
 file_name = path + '/data/trials.json'
 with open(file_name, "w") as f:
     f.write(res.text)
+    f.close()
 file_name = path + '/content/api/trials.json'
 with open(file_name, "w") as f:
     f.write(res.text)
+    f.close()
 ## Save excel versions
 import pandas as pd
 import html
@@ -42,6 +47,37 @@ trials_json = pd.read_json('data/trials.json')
 trials_json.link = trials_json.link.apply(html.unescape)
 trials_json.summary = trials_json.summary.apply(html.unescape)
 trials_json.to_excel('content/api/trials.xlsx')
+
+# Make sure directory exists or create it
+articlesDir = path + "/content/articles/"
+articlesDirExists = pathlib.Path(articlesDir)
+
+if articlesDirExists.exists() == False:
+    articlesDirExists.mkdir(parents=True, exist_ok=True)
+
+# open articles.json
+articles = path + '/data/articles.json'
+with open(articles,"r") as a:
+    data = a.read()
+
+jsonArticles = json.loads(data)
+
+for article in jsonArticles:
+    # for each record, write a file
+    markdownDir = pathlib.Path(articlesDir+str(article["article_id"]))
+    markdownDir.mkdir(parents=True, exist_ok=True)
+
+    with open(str(markdownDir)+"/index.md", "w+") as f:
+        articledata = "---\narticle_id: " + str(article["article_id"]) + "\ndiscovery_date: " + str(article["discovery_date"]) + "\ntitle: \'" + article["title"] + "\'\nsummary: \'" + article["summary"] + "\'\nlink: \'" + article["link"] + "\'\npublished_date: " + str(article["published_date"]) + "\nsource: " + article["source"] + "\nrelevant: " + \
+            str(article["relevant"]) + "\n---\n" + article["summary"]
+        # add content to file
+
+        f.write(articledata)
+        f.close()
+
+
+
+
 args = ("/usr/local/bin/hugo", "-d", website_path,"--cacheDir", path)
 popen = subprocess.Popen(args, stdout=subprocess.PIPE, universal_newlines=True)
 popen.wait()
