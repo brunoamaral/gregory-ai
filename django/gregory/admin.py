f7dfb630f01256c68df555ad831ccb0bda04c1c6
Bruno Amaral <mail@brunoamaral.eu>
2022-03-05 15:53:27 +0000

    just making the admin page a bit nicer

--- django/gregory/admin.py
+++ django/gregory/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Articles, Trials, Sources
+from .models import Articles, Categories, Trials, Sources, Entities
 
 # this class define which department columns will be shown in the department admin web site.
 class ArticleAdmin(admin.ModelAdmin):
     # a list of displayed columns name.
     list_display = ['article_id', 'title']
 
+class TrialAdmin(admin.ModelAdmin):
+    # a list of displayed columns name.
+    list_display = ['trial_id', 'title']
+
+class SourceAdmin(admin.ModelAdmin):
+    # a list of displayed columns name.
+    list_display = ['name','subject']
+
 admin.site.register(Articles,ArticleAdmin)
-admin.site.register(Trials)
-admin.site.register(Sources)
+admin.site.register(Trials, TrialAdmin)
+admin.site.register(Sources,SourceAdmin)
+admin.site.register(Categories)
+admin.site.register(Entities)
--- django/gregory/models.py
+++ django/gregory/models.py
@@ -6,7 +6,18 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+class Categories(models.Model):
+	category_id = models.AutoField(primary_key=True)
+	category_name = models.TextField(blank=True, null=True)
 
+
+	def __str__(self):
+		return self.category_name
+
+	class Meta:
+		managed = False
+		db_table = 'categories'
+		verbose_name_plural = 'categories'
 
 class Articles(models.Model):
 	article_id = models.AutoField(primary_key=True)
@@ -22,7 +33,8 @@ class Articles(models.Model):
 	sent_to_subscribers = models.BooleanField(blank=True, null=True)
 	discovery_date = models.DateTimeField()
 	sent_to_twitter = models.BooleanField(blank=True, null=True)
-	noun_phrases = models.JSONField(blank=True, null=True)  # This field type is a guess.
+	noun_phrases = models.JSONField(blank=True, null=True)  
+	# categories = models.ManyToManyField(Categories)
 
 	def __str__(self):
 		return str(self.article_id)
@@ -33,14 +45,7 @@ class Articles(models.Model):
 		verbose_name_plural = 'articles'
 
 
-class Categories(models.Model):
-	category_id = models.AutoField(primary_key=True)
-	category_name = models.TextField(blank=True, null=True)
 
-	class Meta:
-		managed = False
-		db_table = 'categories'
-		verbose_name_plural = 'categories'
 
 
 class Entities(models.Model):
@@ -110,7 +115,7 @@ class Trials(models.Model):
 	sent_to_subscribers = models.BooleanField(blank=True, null=True)
 
 	def __str__(self):
-		return str(self.trial_id) + ': ' + self.title
+		return str(self.trial_id) 
 
 	class Meta:
 		managed = False
