df5b72d39edb6d6c55d4d196e82c2f5b4ce13c95
Bruno Amaral <mail@brunoamaral.eu>
2022-03-05 10:40:08 +0000

    show article id and title

--- django/gregory/admin.py
+++ django/gregory/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Articles, Trials, Sources
 
-admin.site.register(Articles)
+# this class define which department columns will be shown in the department admin web site.
+class ArticleAdmin(admin.ModelAdmin):
+    # a list of displayed columns name.
+    list_display = ['article_id', 'title']
+
+admin.site.register(Articles,ArticleAdmin)
 admin.site.register(Trials)
 admin.site.register(Sources)
