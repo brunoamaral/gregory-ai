8f9d90047c06ab9ca97afc372c83e84c0bc5ece2
Bruno Amaral <mail@brunoamaral.eu>
2022-03-08 18:34:25 +0000

    use django migrations to manage the db

--- django/gregory/models.py
+++ django/gregory/models.py
@@ -4,20 +4,21 @@
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 class Categories(models.Model):
 	category_id = models.AutoField(primary_key=True)
 	category_name = models.TextField(blank=True, null=True)
 
+	# article = models.ManyToManyField('Articles', through='RelArticlesCategories',related_name='article',related_query_name='rel_articles_categories.article_id')
 
 	def __str__(self):
 		return self.category_name
 
 	class Meta:
-		managed = False
-		db_table = 'categories'
+		managed = True
 		verbose_name_plural = 'categories'
+		db_table = 'categories'
+
 
 class Articles(models.Model):
 	article_id = models.AutoField(primary_key=True)
@@ -27,63 +28,43 @@ class Articles(models.Model):
 	published_date = models.DateTimeField(blank=True, null=True)
 	source = models.ForeignKey('Sources', models.DO_NOTHING, db_column='source', blank=True, null=True)
 	relevant = models.BooleanField(blank=True, null=True)
-	sent_to_admin = models.BooleanField(blank=True, null=True)
 	ml_prediction_gnb = models.BooleanField(blank=True, null=True)
 	ml_prediction_lr = models.BooleanField(blank=True, null=True)
-	sent_to_subscribers = models.BooleanField(blank=True, null=True)
 	discovery_date = models.DateTimeField()
-	sent_to_twitter = models.BooleanField(blank=True, null=True)
 	noun_phrases = models.JSONField(blank=True, null=True)
-	#categories = models.ManyToManyField(RelArticlesCategories)
+	# category = models.ManyToManyField(Categories, ,related_query_name='rel_articles_categories.category_id')
+	sent_to_admin = models.BooleanField(blank=True, null=True)
+	sent_to_subscribers = models.BooleanField(blank=True, null=True)
+	sent_to_twitter = models.BooleanField(blank=True, null=True)
+	categories = models.ManyToManyField(Categories, 
+		# through='RelArticlesCategories',
+		# through_fields=('article_id', 'category_id'),
+		# related_query_name='rel_articles_categories.category_id',
+		# related_name='+',
+		)
+	entities = models.ManyToManyField('Entities')
+
 
 	def __str__(self):
 		return str(self.article_id)
 
 	class Meta:
-		managed = False
-		db_table = 'articles'
+		managed = True
 		unique_together = (('title', 'link'),)
 		verbose_name_plural = 'articles'
-
-
-
+		db_table = 'articles'
 
 
 class Entities(models.Model):
 	entity = models.TextField()
 	label = models.TextField()
 
+
 	class Meta:
-		managed = False
-		db_table = 'entities'
+		managed = True
 		verbose_name_plural = 'entities'
+		db_table = 'entities'
 
-
-class RelArticlesCategories(models.Model):
-	article = models.ForeignKey(Articles, models.DO_NOTHING)	
-	category = models.ForeignKey(Categories, models.DO_NOTHING)
-
-	class Meta:
-		managed = False
-		db_table = 'rel_articles_categories'
-
-
-class RelArticlesEntities(models.Model):
-	entity = models.ForeignKey(Entities, models.DO_NOTHING, blank=True, null=True)
-	article = models.ForeignKey(Articles, models.DO_NOTHING, blank=True, null=True)
-
-	class Meta:
-		managed = False
-		db_table = 'rel_articles_entities'
-
-
-class RelArticlesSources(models.Model):
-	article = models.ForeignKey(Articles, models.DO_NOTHING, blank=True, null=True)
-	source = models.ForeignKey('Sources', models.DO_NOTHING, blank=True, null=True)
-
-	class Meta:
-		managed = False
-		db_table = 'rel_articles_sources'
 
 
 class Sources(models.Model):
@@ -93,14 +74,15 @@ class Sources(models.Model):
 	language = models.TextField()
 	subject = models.TextField()
 	method = models.TextField()
+	
 
 	def __str__(self):
 		return self.name
 
 	class Meta:
-		managed = False
-		db_table = 'sources'
+		managed = True
 		verbose_name_plural = 'sources'
+		db_table = 'sources'
 
 class Trials(models.Model):
 	trial_id = models.AutoField(primary_key=True)
@@ -119,7 +101,8 @@ class Trials(models.Model):
 		return str(self.trial_id) 
 
 	class Meta:
-		managed = False
-		db_table = 'trials'
+		managed = True
 		unique_together = (('title', 'link'),)
 		verbose_name_plural = 'trials'
+		db_table = 'trials'
+
