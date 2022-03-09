93f3822fae1b7827c965baf0c9bfe1c03a969b88
Bruno Amaral <mail@brunoamaral.eu>
2022-03-09 18:30:52 +0000

    clean up code

--- django/gregory/models.py
+++ django/gregory/models.py
@@ -1,9 +1,3 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 from django.db import models
 class Categories(models.Model):
 	category_id = models.AutoField(primary_key=True)
