08f94cf77c3824f77752a97774eaa0d73dac16dd
Bruno Amaral <mail@brunoamaral.eu>
2022-03-13 20:49:32 +0000

    adds doi number to database, solves #72

--- python-ml/feedreader.py
+++ python-ml/feedreader.py
@@ -24,8 +24,9 @@ cur.execute("SELECT source_id,name,link,subject FROM sources WHERE method = 'rss
 sources = cur.fetchall()
 
 for i in sources:
-	link = i[2]
 	source_id = i[0]
+	source_name = i[1]
+	link = i[2]
 	d = feedparser.parse(link)
 	for entry in d['entries']:
 		summary = ''
@@ -38,13 +39,21 @@ for i in sources:
 			published = entry['published']
 		else:
 			published = entry['prism_coverdate']
+		###
+		# This is a bad solution but it will have to do for now
+		###
+		doi = None 
+		if source_name == 'PubMed':
+			doi = entry['dc_identifier'].replace('doi:','')
+		if source_name == 'FASEB':
+			doi = entry['prism_doi']
 		with conn:
 			try:
 				cur.execute("""
-				INSERT INTO articles (discovery_date,title,summary,link,published_date,source)
-				VALUES ( current_timestamp, %(article_title)s, %(article_summary)s, %(article_link)s, %(article_pubdate)s, %(source_id)s );
+				INSERT INTO articles (discovery_date,title,summary,link,published_date,source,doi)
+				VALUES ( current_timestamp, %(article_title)s, %(article_summary)s, %(article_link)s, %(article_pubdate)s, %(source_id)s, %(doi)s );
 					""",
-					{'article_title': entry['title'], 'article_summary': summary, 'article_link': entry['link'], 'article_pubdate': published, 'source_link': link, 'source_id': source_id })
+					{'article_title': entry['title'], 'article_summary': summary, 'article_link': entry['link'], 'article_pubdate': published, 'source_link': link, 'source_id': source_id, 'doi': doi })
 				print(entry['title'])
 			except Exception as e: print(e)
 			finally:
@@ -87,3 +96,4 @@ for i in sources:
 			finally:
 				if conn.closed == 1:
 					conn.close()
+
