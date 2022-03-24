88e5ce454619ffefb95cc6432cce6de5f6683187
Bruno Amaral <mail@brunoamaral.eu>
2022-03-24 14:36:12 +0000

    add custom templates

--- django/admin/settings.py
+++ django/admin/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'admin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
