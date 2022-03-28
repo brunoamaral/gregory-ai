1987bb7d78ad66e79089c68a99ae90a6ce7b5bb4
Bruno Amaral <mail@brunoamaral.eu>
2022-03-28 01:05:00 +0100

    add search to views

--- django/api/views.py
+++ django/api/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from django.shortcuts import get_object_or_404, render
 from gregory.models import Articles, Trials, Sources
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework import generics
+from rest_framework import viewsets, permissions, generics, filters
+from django.http import HttpResponse
 
 from api.serializers import ArticleSerializer, TrialSerializer, SourceSerializer
 
-# Create your views here.
+###
+# ARTICLES
+### 
 class ArticleViewSet(viewsets.ModelViewSet):
 	"""
 	List all articles in the database by published date
@@ -14,6 +16,18 @@ class ArticleViewSet(viewsets.ModelViewSet):
 	queryset = Articles.objects.all().order_by('-published_date')
 	serializer_class = ArticleSerializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+	filter_backends = [filters.SearchFilter]
+	search_fields  = ['title','summary']
+
+class RelatedArticles(viewsets.ModelViewSet):
+	"""
+	Search related articles by the noun_phrases field
+	"""
+	queryset = Articles.objects.all().order_by('-published_date')
+	serializer_class = ArticleSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+	filter_backends = [filters.SearchFilter]
+	search_fields  = ['$noun_phrases']
 
 class AllArticleViewSet(generics.ListAPIView):
 	"""
@@ -23,35 +37,6 @@ class AllArticleViewSet(generics.ListAPIView):
 	queryset = Articles.objects.all().order_by('-published_date')
 	serializer_class = ArticleSerializer
 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-
-class TrialViewSet(viewsets.ModelViewSet):
-	"""
-	List all clinical trials by discovery date
-	"""
-	queryset = Trials.objects.all().order_by('-discovery_date')
-	serializer_class = TrialSerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-class AllTrialViewSet(generics.ListAPIView):
-	"""
-	List all clinical trials by discovery date
-	"""
-	pagination_class = None
-	queryset = Trials.objects.all().order_by('-discovery_date')
-	serializer_class = TrialSerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-class SourceViewSet(viewsets.ModelViewSet):
-	"""
-	List all sources of data
-	"""
-	queryset = Sources.objects.all().order_by('name')
-	serializer_class = SourceSerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
 
 class RelevantList(generics.ListAPIView):
 	serializer_class = ArticleSerializer
@@ -69,7 +54,7 @@ class UnsentList(generics.ListAPIView):
 	serializer_class = ArticleSerializer
 
 	def get_queryset(self):
-		return Articles.objects.all().exclude(sent_to_subscribers =True)
+		return Articles.objects.all().exclude(sent_to_subscribers = True)
 
 class ArticlesBySourceList(generics.ListAPIView):
 	"""
@@ -82,6 +67,68 @@ class ArticlesBySourceList(generics.ListAPIView):
 		source = self.kwargs['source']
 		return Articles.objects.filter(source=source)
 
+# class ArticleRelevant(generics.RetrieveUpdateAPIView):
+# 	"""
+# 	Change the value of relevancy of the article
+# 	"""
+# 	http_method_names = ['get','put']
+# 	serializer_class = ArticleSerializer
+# 	lookup_field = 'article_id'
+# 	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+# 	def get_queryset(self):
+# 		return Articles.objects.filter(article_id = self.kwargs['article_id'])
+
+# 	def update(self,request,article_id=None,*args, **kwargs):
+# 		instance = self.get_object()
+# 		value = self.request.data.get("value", None)  # read data from request
+
+# 		if value == 1:
+# 			value = True
+# 		elif value == 0:
+# 			value = False
+# 		data = {"relevant": value}
+# 		serializer = ArticleSerializer(instance,data,partial=True)
+# 		if serializer.is_valid():
+# 			serializer.save()
+# 			return HttpResponse(serializer.data)
+# 		else: 
+# 			return HttpResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ArticlesByKeyword(generics.ListAPIView):
+	"""
+	List articles by keyword
+	"""
+	serializer_class = ArticleSerializer
+	permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+	filter_backends = [filters.SearchFilter]
+	search_fields = ['title','summary']
+	def get_queryset(self):
+		return Articles.objects.filter(title='.*ocrevus.*')
+###
+# TRIALS
+### 
+
+class TrialViewSet(viewsets.ModelViewSet):
+	"""
+	List all clinical trials by discovery date
+	"""
+	queryset = Trials.objects.all().order_by('-discovery_date')
+	serializer_class = TrialSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+	filter_backends = [filters.SearchFilter]
+	search_fields  = ['title','summary']
+
+
+class AllTrialViewSet(generics.ListAPIView):
+	"""
+	List all clinical trials by discovery date
+	"""
+	pagination_class = None
+	queryset = Trials.objects.all().order_by('-discovery_date')
+	serializer_class = TrialSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
 class TrialsBySourceList(generics.ListAPIView):
 	serializer_class = ArticleSerializer
 
@@ -92,13 +139,17 @@ class TrialsBySourceList(generics.ListAPIView):
 		source = self.kwargs['source']
 		return Trials.objects.filter(source=source)
 
-# class PurchaseList(generics.ListAPIView):
-#     serializer_class = PurchaseSerializer
 
-#     def get_queryset(self):
-#         """
-#         This view should return a list of all the purchases for
-#         the user as determined by the username portion of the URL.
-#         """
-#         username = self.kwargs['username']
-#         return Purchase.objects.filter(purchaser__username=username)
+###
+# SOURCES
+### 
+
+class SourceViewSet(viewsets.ModelViewSet):
+	"""
+	List all sources of data
+	"""
+	queryset = Sources.objects.all().order_by('name')
+	serializer_class = SourceSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
