bb4519f6654704d7bfa4f46c895bbf3675e896ea
Bruno Amaral <mail@brunoamaral.eu>
2022-03-30 23:59:21 +0100

    apply gzip middleware

--- django/admin/settings.py
+++ django/admin/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-
 ]
 
 MIDDLEWARE = [
@@ -51,6 +50,7 @@ MIDDLEWARE = [
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.gzip.GZipMiddleware',
 ]
 
 ROOT_URLCONF = 'admin.urls'
