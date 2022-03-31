c9c1468b6143fe99b2e7aa6753da2faaaf5c88d1
Bruno Amaral <mail@brunoamaral.eu>
2022-03-31 20:27:18 +0100

    include django-rest urls

--- django/admin/urls.py
+++ django/admin/urls.py
@@ -26,6 +26,7 @@ router.register(r'sources', views.SourceViewSet)
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
+	path('api-auth/', include('rest_framework.urls')),
 	path('articles/all/', views.AllArticleViewSet.as_view()),
 	path('trials/all/', views.AllTrialViewSet.as_view()),
 	re_path('^articles/relevant/$', views.RelevantList.as_view()),
