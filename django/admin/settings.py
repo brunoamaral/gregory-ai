d6f1c4b7e8b200025c11eae181246124c4129164
Bruno Amaral <mail@brunoamaral.eu>
2022-04-01 11:10:30 +0100

    static path

--- django/admin/settings.py
+++ django/admin/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = '/code/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
