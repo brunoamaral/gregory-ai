0c6992817fae89b76d56bca3a8faeafb638a7a42
Bruno Amaral <mail@brunoamaral.eu>
2022-04-10 17:25:51 +0100

    cron jobs are working

--- django/subscriptions/mercury.py
+++ django/subscriptions/mercury.py
@@ -1,4 +1,4 @@
-from admin.settings import * 
+from django.conf import settings
 from subscriptions.models import Subscribers,Lists
 from django_cron import CronJobBase, Schedule
 
@@ -6,30 +6,30 @@ list_clinical_trials = []
 for email in Subscribers.objects.filter(lists__list_name='Clinical Trials').values():
 	list_clinical_trials.append(email['email'])
 
-
 list_articles = []
 for email in Subscribers.objects.filter(lists__list_name='Articles').values():
 	list_articles.append(email['email'])
 
 import requests
-def send_simple_message(to,bcc,subject, text,html):
-		return requests.post(
-				EMAIL_MAILGUN_API_URL,
-				auth=("api", EMAIL_MAILGUN),
-				data={"from": "Greg <greg@mg.gregory-ms.com>",
-							"to": to,
-							"bcc": bcc,
-							"subject": subject,
-							"text": text,
-							"html": html})
-
+def send_simple_message(to=None,bcc=None,subject='Test', text=None,html=None, email_mailgun_api_url=settings.EMAIL_MAILGUN_API_URL, email_mailgun_api=settings.EMAIL_MAILGUN_API):
+	return requests.post(
+			email_mailgun_api_url,
+			auth=("api", email_mailgun_api),
+			data={"from": "Greg <greg@mg.gregory-ms.com>",
+						"to": to,
+						"bcc": bcc,
+						"subject": subject,
+						"text": text,
+						"html": html
+						}
+						)
 
 class MyCronJob(CronJobBase):
-		RUN_EVERY_MINS = 1 # every minute
+	RUN_EVERY_MINS = 1 # every minute
 
-		schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-		code = 'subscriptions.my_cron_job'    # a unique code
+	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+	code = 'subscriptions.my_cron_job'    # a unique code
 
-		def do(self):
-			send_simple_message(list_articles,'list_articles','this is a test 2248')
-			pass
+	def do(self):
+		send_simple_message(to='greg@gregory-ms.com',bcc=list_articles,subject='list_articles',text='this is a test 1716')
+		pass
