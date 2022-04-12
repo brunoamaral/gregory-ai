2b069d67b25b506dc0c19e06193610269715a8cf
Bruno Amaral <mail@brunoamaral.eu>
2022-04-13 00:12:10 +0100

    move files into place

--- python-ml/utils/model_utils.py
+++
@@ -1,8 +0,0 @@
-from sklearn.base import TransformerMixin
-
-# This is an util function to be used in the GaussianNB pipeline
-class DenseTransformer(TransformerMixin):
-    def fit(self, X, y=None, **fit_params):
-        return self
-    def transform(self, X, y=None, **fit_params):
-        return X.todense()
