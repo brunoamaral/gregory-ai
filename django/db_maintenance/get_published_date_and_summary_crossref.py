c03cfa9cdc72c98f34bbc82f2f8051581398582a
Bruno Amaral <mail@brunoamaral.eu>
2022-06-01 13:46:37 +0100

    add new maintenance task for DOI,abstract,and published_date

---
+++ django/db_maintenance/get_published_date_and_summary_crossref.py
@@ -0,0 +1,29 @@
+from crossref.restful import Works, Etiquette
+from django_cron import CronJobBase, Schedule
+from gregory.models import Articles,Trials
+from django.db.models import Q
+from datetime import datetime
+import pytz
+
+class GetDateSummaryCrossRef(CronJobBase):
+	RUN_EVERY_MINS = 300 # every 5h
+	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+	code = 'db_maintenance.get_crossref'    # a unique code
+
+	def do(self):
+		my_etiquette = Etiquette('Gregory MS', 'v8', 'https://gregory-ms.com', 'bruno@gregory-ms.com')
+		works = Works(etiquette=my_etiquette)
+		articles = Articles.objects.filter(published_date=None)
+		timezone = pytz.timezone('UTC')
+		for article in articles:
+			if hasattr(article,'doi') and article.doi != None:
+				w = works.doi(article.doi)
+				issued = w['issued']['date-parts'][0]
+				published_date = datetime( year=issued[0], month=issued[1], day=issued[2], tzinfo=timezone)
+				article.published_date = published_date
+				if article.summary.length() < 50 or article.summary == None:
+					try:
+						article.summary = w['abstract']
+					except:
+						pass
+				article.save()
