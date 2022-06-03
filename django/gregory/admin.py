b4cb54ff4df418a9c78ba7eb0d7e9a4e2d2366fb
Bruno Amaral <mail@brunoamaral.eu>
2022-06-03 15:41:04 +0100

    better widget to manage authors

--- django/gregory/admin.py
+++ django/gregory/admin.py
@@ -9,6 +9,8 @@ class ArticleAdmin(admin.ModelAdmin):
 	list_display = ['article_id', 'title','source']
 	readonly_fields = ['ml_prediction_gnb','ml_prediction_lr','categories','entities']
 	search_fields = ['article_id', 'title','doi' ]
+	filter_horizontal = ('authors',)
+
 
 class TrialAdmin(admin.ModelAdmin):
 	# a list of displayed columns name.
