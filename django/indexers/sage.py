64724f0c624e4017400eb93cb17d33174de8ffb1
Bruno Amaral <mail@brunoamaral.eu>
2022-06-09 11:33:12 +0100

    slight improvement to the import script

--- django/indexers/sage.py
+++ django/indexers/sage.py
@@ -1,18 +1,18 @@
-from gregory.models import Articles
+from gregory.models import Articles, Sources
 from datetime import datetime
 import json
 
-f = open('/code/indexers/input/sagepub.json')
+f = open('/code/indexers/input/sagepub-1995-2010.json')
 input = json.load(f)
 
 discovery_date = datetime.now()
+source = Sources.objects.get(pk=9)
 for i in input:
 	try: 
 		# print(i)
 		title = i['title']
 		link = i['link']
 		doi = i['doi']
-		source = 9
 		article = Articles.objects.create(
 			title = title,
 			link = link,
@@ -21,5 +21,5 @@ for i in input:
 			source = source,
 		)
 	except:
-		print('not unique?')
+		print('not unique?::', doi, title)
 		pass
