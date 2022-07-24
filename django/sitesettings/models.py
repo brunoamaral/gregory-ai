e7ac1365f635afea34ff1246074315f9c537c111
Bruno Amaral <mail@brunoamaral.eu>
2022-07-24 11:54:04 +0100

    moves content from template system to the db

--- django/sitesettings/models.py
+++ django/sitesettings/models.py
@@ -5,4 +5,5 @@ from django.contrib.sites.models import Site
 class CustomSetting(models.Model):
 	setting_id = models.AutoField(primary_key=True)
 	site = models.ForeignKey(Site, on_delete=models.PROTECT)
-	title = models.TextField(blank=False, null=False, unique=True)
+	title = models.CharField(max_length=280,blank=False, null=False, unique=True)
+	email_footer = models.TextField(blank=True,null=True)
