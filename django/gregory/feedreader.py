bcb561c6fab47eb5b807930acf989682ee7ca733
Bruno Amaral <mail@brunoamaral.eu>
2022-07-29 16:27:16 +0100

    corrections and cleanup of feedreader task

--- django/gregory/feedreader.py
+++ django/gregory/feedreader.py
@@ -1,16 +1,16 @@
 import feedparser
-import psycopg2
+from dateutil.parser import parse
+from datetime import datetime
 from dotenv import load_dotenv
-import os
 import ssl
-from models import Articles,Sources
+from .models import Articles,Trials,Sources
 from django_cron import CronJobBase, Schedule
 
 load_dotenv()
 
 
 class FeedReaderTask(CronJobBase):
-	RUN_EVERY_MINS = 5
+	RUN_EVERY_MINS = 1
 	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
 	code = 'gregory.feedreadertask'    # a unique code
 
@@ -21,7 +21,6 @@ class FeedReaderTask(CronJobBase):
 		sources = Sources.objects.filter(method='rss',source_for='science paper')
 
 		for i in sources:
-			source_id = i.source_id
 			source_name = i.name
 			source_for = i.source_for
 			link = i.link
@@ -34,9 +33,9 @@ class FeedReaderTask(CronJobBase):
 					summary = entry['summary']
 				published = entry.get('published')
 				if published:
-					published = entry['published']
+					published = parse(entry['published'])
 				else:
-					published = entry['prism_coverdate']
+					published = parse(entry['prism_coverdate'])
 				###
 				# This is a bad solution but it will have to do for now
 				###
@@ -45,17 +44,19 @@ class FeedReaderTask(CronJobBase):
 					doi = entry['dc_identifier'].replace('doi:','')
 				if source_name == 'FASEB':
 					doi = entry['prism_doi']
-				science_paper = Articles.objects.create(
-					title = entry['title'], summary = summary, link = entry['link'], published_date = published, source_link = link, source = source_id, doi = doi, kind = source_for
-				)
+				try:
+					science_paper = Articles.objects.create(
+					discovery_date=datetime.now(), title = entry['title'], summary = summary, link = entry['link'], published_date = published, source = i, doi = doi, kind = source_for )
+				except:
+					pass
+
+
 
 
 		###
 		# GET TRIALS
 		###
 
-		# INSERT INTO trials (discovery_date,title,summary,link,published_date,source,relevant)
-		# VALUES (current_timestamp,'{{article.title}}','{{article.description}}','{{{topic}}}','{{article.pubdate}}','{{article.source}}',NULL)
 		sources = Sources.objects.filter(method='rss',source_for='trials')
 
 		# This disables the SSL verification. The only reason why we are doing this is because of issue #55 <https://github.com/brunoamaral/gregory/issues/55> 
@@ -74,9 +75,10 @@ class FeedReaderTask(CronJobBase):
 					summary = entry['summary']
 				published = entry.get('published')
 				if published:
-					published = entry['published']
-				trial = Trials.objects.create(
-					title = entry['title'], summary = summary, link = entry['link'], published_date = published, link = link, source = source_id
-				) 
+					published = parse(entry['published'])
+				try:
+						trial = Trials.objects.create( discovery_date=datetime.now(), title = entry['title'], summary = summary, link = entry['link'], published_date = published, source = i) 
+				except:
+						pass
 
 		pass
