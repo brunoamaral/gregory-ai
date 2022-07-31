e21b6062a872b4696594fbba547b44f67f872136
Bruno Amaral <mail@brunoamaral.eu>
2022-07-31 23:43:02 +0100

    minor edit to cron

--- django/gregory/feedreader.py
+++ django/gregory/feedreader.py
@@ -8,7 +8,7 @@ import requests
 
 
 class FeedReaderTask(CronJobBase):
-	RUN_EVERY_MINS = 1 #30
+	RUN_EVERY_MINS = 30
 	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
 	code = 'gregory.feedreadertask'    # a unique code
 
