8191888762f16d11b567e37630ff3034abc657bd
Bruno Amaral <mail@brunoamaral.eu>
2022-08-07 00:49:31 +0100

    improved the get published_date routine

--- django/db_maintenance/get_published_date_and_summary_crossref.py
+++ django/db_maintenance/get_published_date_and_summary_crossref.py
@@ -18,10 +18,24 @@ class GetDateSummaryCrossRef(CronJobBase):
 		for article in articles:
 			if hasattr(article,'doi') and article.doi != None:
 				w = works.doi(article.doi)
-				if w.has_key('issued'):
+				if 'issued' in w:
 					issued = w['issued']['date-parts'][0]
+					print(issued)
+					year,month,day = None,1,1
 					try:
-						published_date = datetime( year=issued[0], month=issued[1], day=issued[2], tzinfo=timezone)
+						year = issued[0]
+					except:
+						pass
+					try:
+						month=issued[1]
+					except:
+						pass
+					try:
+						day=issued[2]
+					except:
+						pass
+					try:
+						published_date = datetime( year=year, month=month, day=day, tzinfo=timezone)
 						article.published_date = published_date
 						article.save()
 					except:
