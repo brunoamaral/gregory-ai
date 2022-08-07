234e29d4ee301cdb97f8a9bda1f389908b009331
Bruno Amaral <mail@brunoamaral.eu>
2022-08-07 01:01:03 +0100

    improved the get_doi routine and added more attempts to match

--- django/db_maintenance/get_doi_from_crossref.py
+++ django/db_maintenance/get_doi_from_crossref.py
@@ -4,7 +4,7 @@ from gregory.models import Articles
 import re 
 
 class GetDoiCrossRef(CronJobBase):
-	RUN_EVERY_MINS = 600 # every 10h
+	RUN_EVERY_MINS = 300 # every 5h
 	schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
 	code = 'db_maintenance.get_doi_crossref'    # a unique code
 
@@ -18,18 +18,18 @@ class GetDoiCrossRef(CronJobBase):
 				i = 0
 				work = works.query(bibliographic=article.title).sort('relevance')
 				for w in work:
-					crossref_title = ''
-					article_title = re.sub(r'[^A-Za-z0-9 ]+', '', article.title)
-					article_title = re.sub(r' ','',article_title ).lower()
-					crossref_title = re.sub(r'[^A-Za-z0-9 ]+', '', w['title'][0])
-					crossref_title = re.sub(r' ','',crossref_title).lower()
-					print(crossref_title)
-					print(article_title)
-					if crossref_title == article_title:
-						print(article_title)
-						article.doi = w['DOI']
-						article.save()
-					else:
-						i = 1
-					if i == 1:
-						break
+					if 'title' in w:
+						crossref_title = ''
+						article_title = re.sub(r'[^A-Za-z0-9 ]+', '', article.title)
+						article_title = re.sub(r' ','',article_title ).lower()
+						crossref_title = re.sub(r'[^A-Za-z0-9 ]+', '', w['title'][0])
+						crossref_title = re.sub(r' ','',crossref_title).lower()
+						# print(crossref_title)
+						# print(article_title)
+						if crossref_title == article_title:
+							print(article.article_id)
+							article.doi = w['DOI']
+							article.save()
+						i += 1
+						if i == 3:
+							break
