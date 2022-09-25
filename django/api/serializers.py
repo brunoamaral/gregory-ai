2aac855a735caed3b975e58ef6535a1d362acebd
Bruno Amaral <mail@brunoamaral.eu>
2022-09-25 23:18:17 +0100

    list access field on the API

--- django/api/serializers.py
+++ django/api/serializers.py
@@ -9,7 +9,7 @@ class ArticleSerializer(serializers.HyperlinkedModelSerializer):
 
 	class Meta:
 		model = Articles
-		fields = ['article_id','title','summary','link','published_date','source','authors','relevant','ml_prediction_gnb','ml_prediction_lr','discovery_date','noun_phrases','doi']
+		fields = ['article_id','title','summary','link','published_date','source','authors','relevant','ml_prediction_gnb','ml_prediction_lr','discovery_date','noun_phrases','doi','access']
 		read_only_fields = ('discovery_date','ml_prediction_gnb','ml_prediction_lr','noun_phrases')
 		
 class TrialSerializer(serializers.HyperlinkedModelSerializer):
