abf18192858db3093ca2acc7732d44e95443eb3f
Bruno Amaral <mail@brunoamaral.eu>
2022-11-03 11:20:07 +0000

    wip

--- django/gregory/functions.py
+++ django/gregory/functions.py
@@ -1,47 +1,50 @@
-from sitesettings.models import *
-from crossref.restful import Works, Etiquette
-from datetime import datetime
 
+class SciencePaper:
+	SITE = CustomSetting.objects.get(site__domain=os.environ.get('DOMAIN_NAME'))
+	CLIENT_WEBSITE = 'https://' + SITE.site.domain + '/'
+	from datetime import datetime
+	from sitesettings.models import CustomSetting
+	from db_maintenance.unpaywall import unpaywall_utils
+	def __init__(self, doi, SITE=SITE, CLIENT_WEBSITE=CLIENT_WEBSITE):
+		from crossref.restful import Works, Etiquette
+		my_etiquette = Etiquette(SITE.title, 'v8', CLIENT_WEBSITE, SITE.admin_email)
+		works = Works(etiquette=my_etiquette)
+		work = works.doi(doi)
+	def get_access_info(doi,self):
+			article_access = None
+			if bool(doi):
+					if unpaywall_utils.checkIfDOIIsOpenAccess(doi, self.SITE.admin_email):
+						article_access = 'open'
+					else:
+						article_access = 'restricted'
+			print(article_access)
+	def __str__(self):
+		return f"{self.doi}"
 
-SITE = CustomSetting.objects.get(site__domain=os.environ.get('DOMAIN_NAME'))
-CLIENT_WEBSITE = 'https://' + SITE.site.domain + '/'
-my_etiquette = Etiquette(SITE.title, 'v8', CLIENT_WEBSITE, SITE.admin_email)
-works = Works(etiquette=my_etiquette)
+	def get_publisher_and_journal(work):
+		'''
+		returns publisher and container-title as a tuple
+		'''
+		work = works.doi(doi)
+		publisher = None
+		journal = None
+		if work:
+			print(work['publisher'])
+			publisher = work['publisher']
+			try:
+				journal = work['container-title'][0]
+			except IndexError:
+				pass
+		return (publisher,journal)
 
-def get_access_info(doi):
-		article_access = None
-		if bool(doi):
-				if unpaywall_utils.checkIfDOIIsOpenAccess(doi, SITE.admin_email):
-					article_access = 'open'
-				else:
-					article_access = 'restricted'
-		return article_access
-
-
-def get_publisher_and_journal(doi):
-	'''
-	returns publisher and container-title as a tuple
-	'''
-	work = works.doi(doi)
-	publisher = None
-	journal = None
-	if work:
-		print(work['publisher'])
-		publisher = work['publisher']
+	def get_abstract(work):
+		abstract = None
 		try:
-			journal = work['container-title'][0]
-		except IndexError:
-			pass
-	return (publisher,journal)
+				abstract = w['abstract']
+		except:
+				pass
+		return abstract
 
-def get_abstract(doi):
-	abstract = None
-	w = works.doi(doi)
-	try:
-			abstract = w['abstract']
-	except:
-			pass
-	return abstract
 
 
 def get_published_date(doi):
@@ -87,3 +90,28 @@ def get_doi(title):
 			i += 1
 			if i == 5:
 				return None
+
+def get_authors(doi):
+	authors = []
+	work = works.query
+	if 'author' in work and work['author'] is not None:
+		authors = work['author']
+	for author in authors:
+		if 'given' in author and 'family' in author:
+			given_name = None
+			if 'given' in author:
+				given_name = author['given']
+			family_name = None
+			if 'family' in author:
+				family_name = author['family']
+			orcid = None
+			if 'ORCID' in author:
+				orcid = author['ORCID']
+
+			# get or create author
+			author_obj = Authors.objects.get_or_create(given_name=given_name,family_name=family_name,ORCID=orcid)
+			author_obj = author_obj[0]
+			## add to database
+			if author_obj.author_id is not None:
+				# make relationship
+				science_paper.authors.add(author_obj)
