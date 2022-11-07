1c712b914629c649405c7db785e1f516d5a9a3eb
Bruno Amaral <mail@brunoamaral.eu>
2022-11-07 17:41:04 +0000

    configure responses

--- django/api/utils/responses.py
+++ django/api/utils/responses.py
@@ -7,8 +7,11 @@ UNEXPECTED = 0
 NO_API_KEY = 1
 ACCESS_DENIED = 2
 INVALID_API_KEY = 3
-INVALID_API_KEY = 4
-INVALID_IP_ADDRESS = 5
+INVALID_IP_ADDRESS = 4
+SOURCE_NOT_FOUND = 5
+FIELD_NOT_FOUND = 6
+ARTICLE_EXISTS = 7
+ARTICLE_NOT_SAVED = 8
 
 ERRORS = {
     UNEXPECTED: 'Unexpected error. Please contact the support team.',
@@ -16,6 +19,10 @@ ERRORS = {
     ACCESS_DENIED: 'The client does not have access to the requested resource.',
     INVALID_API_KEY: 'The API Key that was provided is invalid.',
     INVALID_IP_ADDRESS: 'The client\'s IP address is not authorized for the provided API Key.',
+    SOURCE_NOT_FOUND: 'The specified source_id did not return any configured source',
+    FIELD_NOT_FOUND: 'One or more fields wasn\'t found in the payload',
+    ARTICLE_EXISTS: 'An article already exists with one of these fields: title, doi.',
+    ARTICLE_NOT_SAVED: 'Could not save the article that was received',
 }
 
 def returnData(data):
