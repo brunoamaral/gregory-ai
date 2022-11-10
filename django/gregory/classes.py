41ece1237768a0386ccb00797369aabf994f4314
Bruno Amaral <mail@brunoamaral.eu>
2022-11-10 20:38:26 +0000

    adds method to populate missing variables

--- django/gregory/classes.py
+++ django/gregory/classes.py
@@ -1,82 +1,90 @@
 class SciencePaper:
-	def __init__(self, doi, title=None):
-		from datetime import datetime
-		from sitesettings.models import CustomSetting
+	def __init__(self, doi=None, title=None,link=None,access=None,publisher=None,journal=None,published_date=None,abstract=None,authors=None):
+		self.doi=doi
+		self.title=title
+		self.link=link
+		self.access=access
+		self.publisher=publisher
+		self.journal=journal
+		self.published_date=published_date
+		self.abstract=abstract
+		self.authors=authors
+	def __str__(self):
+		return f"{self.doi}, {self.title}"
+	def __repr__(self):
+		return f"{self.doi}, \"{self.title}\""
+	def refresh(self):
 		from db_maintenance.unpaywall import unpaywall_utils
 		from crossref.restful import Works, Etiquette
 		import os
 		import pytz
 		import html
 		from bs4 import BeautifulSoup
+		from datetime import datetime
 		timezone = pytz.timezone('UTC')
+		from sitesettings.models import CustomSetting
 		site = CustomSetting.objects.get(site__domain=os.environ.get('DOMAIN_NAME'))
 		client_website = 'https://' + site.site.domain + '/'
 		my_etiquette = Etiquette(site.title, 'v8', client_website, site.admin_email)
 		works = Works(etiquette=my_etiquette)
 		work = None
-		self.doi = None
-		if doi != None:
-			work = works.doi(doi)
-			self.doi = doi
-		self.link = None
-		try: 
-			self.link = work['link'][0]['URL']
-		except: 
-			pass
-		if title == None:
+		if self.doi != None:
+			work = works.doi(self.doi)
+		if self.link == None:
+			try: 
+				self.link = work['link'][0]['URL']
+			except: 
+				pass
+		if self.title == None:
 			try:
 				self.title = work['title'][0]
 			except:
 				pass
-		else:
-			self.title = title
-		article_access = None
-		if bool(doi):
-			if unpaywall_utils.checkIfDOIIsOpenAccess(doi, site.admin_email):
-				article_access = 'open'
+		if self.doi != None and self.access == None:
+			if unpaywall_utils.checkIfDOIIsOpenAccess(self.doi, site.admin_email):
+				self.access = 'open'
 			else:
-				article_access = 'restricted'
-		self.access = article_access
-		self.publisher = None
-		if work != None and 'publisher' in work:
-			if isinstance(work['publisher'],list):
-				self.publisher = work['publisher'][0]
-			else:
-				self.publisher = work['publisher']
-		self.journal = None
-		if work != None and 'container-title' in work:
-			if isinstance(work['container-title'], list):
-				if len(work['container-title']) > 0:
-					self.journal = work['container-title'][0]
+				self.access = 'restricted'
+		if self.publisher == None:
+			if work != None and 'publisher' in work:
+				if isinstance(work['publisher'],list):
+					self.publisher = work['publisher'][0]
 				else:
-					self.journal = ''
-			else:
-				self.journal = work['container-title']
-		self.published_date = None
-		if work != None and 'issued' in work:
-			issued = work['issued']['date-parts'][0]
-		year,month,day = None,1,1
-		try:
-			year = issued[0]
-		except:
-			pass
-		try:
-			month=issued[1]
-		except:
-			pass
-		try:
-			day=issued[2]
-		except:
-			pass
-		try:
-			self.published_date = datetime( year=year, month=month, day=day, tzinfo=timezone)
-		except:
+					self.publisher = work['publisher']
+		if self.journal == None:
+			if work != None and 'container-title' in work:
+				if isinstance(work['container-title'], list):
+					if len(work['container-title']) > 0:
+						self.journal = work['container-title'][0]
+					else:
+						self.journal = ''
+				else:
+					self.journal = work['container-title']
+		if self.published_date == None:
+			if work != None and 'issued' in work:
+				issued = work['issued']['date-parts'][0]
+			year,month,day = None,1,1
+			try:
+				year = issued[0]
+			except:
 				pass
-		self.abstract = None
-		try:
-			self.abstract = work['abstract']
-		except:
-			pass
+			try:
+				month=issued[1]
+			except:
+				pass
+			try:
+				day=issued[2]
+			except:
+				pass
+			try:
+				self.published_date = datetime( year=year, month=month, day=day, tzinfo=timezone)
+			except:
+					pass
+		if self.abstract == None:
+			try:
+				self.abstract = work['abstract']
+			except:
+				pass
 		if self.abstract != None:
 			self.abstract = html.unescape(self.abstract)
 			soup = BeautifulSoup(self.abstract,'html.parser')
@@ -84,12 +92,8 @@ class SciencePaper:
 				for attribute in ["class", "id", "name", "style"]:
 					del tag[attribute]
 			self.abstract = str(soup)
-		self.authors = None
-		try:
-			self.authors = work['author']
-		except:
-			pass
-	def __str__(self):
-		return f"{self.doi}, {self.title}"
-	def __repr__(self):
-		return f"{self.doi}, \"{self.title}\""
+		if self.authors == None:
+			try:
+				self.authors = work['author']
+			except:
+				pass
