90fff5b130090d882e79b7a9a41f298775ca2cdf
Bruno Amaral <mail@brunoamaral.eu>
2023-03-26 12:51:08 +0100

    chatGPT did all the work

--- django/gregory/management/commands/get_takeaways.py
+++ django/gregory/management/commands/get_takeaways.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import html
-import pandas as pd
 import time
 from django.core.management.base import BaseCommand
 from django.db.models.functions import Length
@@ -20,12 +19,12 @@ class Command(BaseCommand):
 			return min(max_length, int(nr_words / 2))
 
 	@staticmethod
-	def summarize_abstract(row, summarizer, min_length=25):
+	def summarize_abstract(article_id, abstract, summarizer, min_length=25):
 			start = time.time()
-			max_length = Command.get_summary_max_length(row['abstract'])
+			max_length = Command.get_summary_max_length(abstract)
 			if max_length > min_length:
-					print(f"Summarizing abstract #{row['article_id']} with lengths [{min_length}, {max_length}]")
-					summary = summarizer(row['abstract'], min_length=min_length, max_length=max_length)
+					print(f"Summarizing abstract {article_id} with lengths [{min_length}, {max_length}]")
+					summary = summarizer(abstract, min_length=min_length, max_length=max_length)
 					end = time.time()
 					print(f" => Ellapsed time: {end - start} sec.")
 					return summary[0]['summary_text']
@@ -39,23 +38,16 @@ class Command(BaseCommand):
 					takeaways=None
 			)[:1]
 
-			dataset = pd.DataFrame(list(queryset.values("article_id", "summary")))
-
-			if dataset.empty:
-					print('Nothing to analyse, dataset is empty.')
+			if not queryset:
+					print('Nothing to analyse, queryset is empty.')
 					exit()
 
-			dataset = dataset.rename(columns={"summary": "abstract"})
-			dataset["abstract"] = dataset["abstract"].apply(html.unescape).apply(self.clean_html)
-			dataset = dataset.replace(r'\n|\r', ' ', regex=True)
-
 			self.stdout.write("Loading the model")
-			summarizer = pipeline("summarization")
+			summarizer = pipeline("summarization", model='philschmid/bart-large-cnn-samsum')
 			self.stdout.write("Summarizer model ready for use")
 
-			dataset['get_takeaways'] = dataset.apply(lambda row: self.summarize_abstract(row, summarizer), axis=1)
-
-			for _, row in dataset.iterrows():
-					article = Articles.objects.get(pk=row['article_id'])
-					article.takeaways = row['get_takeaways']
+			for article in queryset:
+					abstract = self.clean_html(html.unescape(article.summary)).replace('\n', ' ').replace('\r', ' ')
+					takeaways = self.summarize_abstract(article.article_id,abstract, summarizer)
+					article.takeaways = takeaways
 					article.save()
