a3c064b2e4dfd527ea2214fd4c70781f20ede254
Bruno Amaral <mail@brunoamaral.eu>
2023-06-01 00:04:20 +0100

    if a clinical trial exists,based on identifier, update it

--- django/gregory/feedreader.py
+++ django/gregory/feedreader.py
@@ -1,16 +1,18 @@
-import feedparser
-from dateutil.parser import parse
 from .models import Articles,Trials,Sources,Authors
-from django_cron import CronJobBase, Schedule
-import requests
-from sitesettings.models import *
 from crossref.restful import Works, Etiquette
-import os
-import re
-import gregory.functions as greg
-from gregory.classes import SciencePaper, ClinicalTrial
+from dateutil.parser import parse
+from django_cron import CronJobBase, Schedule
+from django.db.models.query_utils import Q as Q  # noqa: F401
 from django.utils import timezone
+from gregory.classes import SciencePaper, ClinicalTrial
+from sitesettings.models import *
+import feedparser
+import gregory.functions as greg
+import os
 import pytz
+import re
+import requests
+
 SITE = CustomSetting.objects.get(site__domain=os.environ.get('DOMAIN_NAME'))
 CLIENT_WEBSITE = 'https://' + SITE.site.domain + '/'
 my_etiquette = Etiquette(SITE.title, 'v8', CLIENT_WEBSITE, SITE.admin_email)
@@ -136,9 +138,40 @@ class FeedReaderTask(CronJobBase):
 				if 'clinicaltrials.gov' in link:
 					nct = entry['guid']
 				identifiers = {"eudract": eudract, "euct": euct, "nct": nct}
-				clinical_trial = ClinicalTrial(title = entry['title'], summary = summary, link = link, published_date = published, identifiers = identifiers,)
+				clinical_trial = ClinicalTrial(title=entry['title'], summary=summary, link=link, published_date=published, identifiers=identifiers)
 				clinical_trial.clean_summary()
+
+				# Get the identifiers
+				nct = clinical_trial.identifiers.get('nct')
+				euct = clinical_trial.identifiers.get('euct')
+				eudract = clinical_trial.identifiers.get('eudract')
+				# Find if there's already a trial with the same identifiers
+				existing_trial = Trials.objects.filter(
+						Q(identifiers__nct=nct) |
+						Q(identifiers__euct=euct) |
+						Q(identifiers__eudract=eudract)
+				).first()
+				if existing_trial:
+					# Update the existing trial
+					existing_trial.discovery_date = timezone.now()
+					existing_trial.title = clinical_trial.title
+					existing_trial.summary = clinical_trial.summary
+					existing_trial.link = clinical_trial.link
+					existing_trial.published_date = clinical_trial.published_date
+					existing_trial.identifiers = clinical_trial.identifiers
+					existing_trial.source = i
+					existing_trial.save()
+			else:
+				# Create a new trial
 				try:
-					trial = Trials.objects.create( discovery_date=timezone.now(), title = clinical_trial.title, summary = clinical_trial.summary, link = clinical_trial.link, published_date = clinical_trial.published_date, identifiers=clinical_trial.identifiers, source = i)
+					trial = Trials.objects.create(
+						discovery_date=timezone.now(),
+						title=clinical_trial.title,
+						summary=clinical_trial.summary,
+						link=clinical_trial.link,
+						published_date=clinical_trial.published_date,
+						identifiers=clinical_trial.identifiers,
+						source=i
+						)
 				except:
 					pass
