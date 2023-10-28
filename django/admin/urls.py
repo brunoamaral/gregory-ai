0950c1da411e35d29cb25287cf278e8f4c72916e
Bruno Amaral <mail@brunoamaral.eu>
2023-10-28 19:27:46 +0100

    rename api endpoint for consistency

--- django/admin/urls.py
+++ django/admin/urls.py
@@ -70,7 +70,7 @@ urlpatterns = [
 	re_path('^articles/source/(?P<source>.+)/$', ArticlesBySourceList.as_view()),
 	re_path('^articles/subject/(?P<subject>.+)/$', ArticlesBySubject.as_view({'get':'list'})),
 	re_path('^articles/journal/(?P<journal>.+)/$', ArticlesByJournal.as_view({'get':'list'})),
-	re_path('^articles/open/$', OpenAccessArticles.as_view()),
+	re_path('^articles/open-access/$', OpenAccessArticles.as_view()),
 	re_path('^articles/unsent/$', UnsentList.as_view()),
 
 	# Relevant articles routes
