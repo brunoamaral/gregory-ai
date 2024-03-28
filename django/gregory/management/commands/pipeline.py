879362555ac5c04b37539aa8734e434eb3744f89
Bruno Amaral <mail@brunoamaral.eu>
2024-03-28 12:42:51 +0000

    use tabs

--- django/gregory/management/commands/pipeline.py
+++ django/gregory/management/commands/pipeline.py
@@ -2,13 +2,12 @@ from django.core.management.base import BaseCommand
 from django.core.management import call_command
 
 class Command(BaseCommand):
-    help = 'Runs a list of specified management commands.'
+	help = 'Runs a list of specified management commands.'
 
-    def handle(self, *args, **options):
-        commands_to_run = ['feedreader', 'update_articles_info', 'get_takeaways', 'get_authors', 'update_orcid', '3_predict']
+	def handle(self, *args, **options):
+		commands_to_run = ['feedreader', 'update_articles_info', 'get_takeaways', 'get_authors', 'update_orcid', '3_predict']
 
-        for cmd in commands_to_run:
-            self.stdout.write(self.style.SUCCESS(f'Running command: {cmd}'))
-            call_command(cmd)
-            self.stdout.write(self.style.SUCCESS(f'Finished command: {cmd}'))
-
+		for cmd in commands_to_run:
+			self.stdout.write(self.style.SUCCESS(f'Running command: {cmd}'))
+			call_command(cmd)
+			self.stdout.write(self.style.SUCCESS(f'Finished command: {cmd}'))
