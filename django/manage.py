d98c24ec5b510044bd9267a955cc76827593f376
Bruno Amaral <mail@brunoamaral.eu>
2024-04-03 21:17:31 +0100

    dirty fix for 3_predict management command to work

--- django/manage.py
+++ django/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from gregory.utils.model_utils import DenseTransformer
 
 def main():
     """Run administrative tasks."""
