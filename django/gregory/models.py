ee270bedf288059de3ef778fbf22b0d3e2992641
Bruno Amaral <mail@brunoamaral.eu>
2024-04-27 16:43:26 +0100

    fix field name

--- django/gregory/models.py
+++ django/gregory/models.py
@@ -47,7 +47,7 @@ class Categories(models.Model):
 		managed = True
 		verbose_name_plural = 'categories'
 		db_table = 'categories'
-		unique_together = (('slug','team'),)
+		unique_together = (('category_slug','team'),)
 
 
 class Entities(models.Model):
