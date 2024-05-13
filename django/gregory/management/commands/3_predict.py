0e98e45bc6a9b93baba0b4d3ef8a00cbf641457e
Bruno Amaral <mail@brunoamaral.eu>
2024-05-13 20:28:37 +0100

    use models in team + subject directories

--- django/gregory/management/commands/3_predict.py
+++ django/gregory/management/commands/3_predict.py
@@ -5,7 +5,7 @@ from datetime import date
 from joblib import load
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from gregory.models import Articles, Subject, MLPredictions
+from gregory.models import Articles, Subject, MLPredictions, Team
 from gregory.utils.model_utils import DenseTransformer
 from gregory.utils.text_utils import cleanHTML, cleanText
 import feedparser
@@ -19,15 +19,21 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		# Initialize models and load pipelines
 		models = ["gnb", "lsvc", "mnb", "lr"]
-		pipelines = {model: load(f'/code/gregory/ml_models/model_{model}.joblib') for model in models}
 
 		today = date.today()
-		dataset_file_csv = f'/code/gregory/data/{today.strftime("%Y-%B")}.csv'
 
 		# Fetch new data
 		subjects = Subject.objects.all()
 		for subject in subjects:
-			print(subject)
+            # Load pipelines if model files exist
+			pipelines = {}
+			for model in models:
+				model_path = f'/code/gregory/ml_models/team_id_{subject.team.pk}/subject_id_{subject.pk}/model_{model}.joblib'
+				if os.path.exists(model_path):
+					pipelines[model] = load(model_path)
+				else:
+					print(f"Model file not found: {model_path}")
+					continue
 			articles_queryset = Articles.objects.filter(ml_predictions__isnull=True, summary__gt=50, subjects=subject)
 			dataset = pd.DataFrame(list(articles_queryset.values("title", "summary", "relevant", "article_id")))
 
@@ -37,7 +43,6 @@ class Command(BaseCommand):
 				dataset["terms"] = dataset["title"].astype(str) + " " + dataset["summary"].astype(str)
 				dataset = dataset[["terms", "relevant", "article_id"]]
 				dataset["relevant"] = dataset["relevant"].fillna(value=0).astype(int)
-				dataset.to_csv(dataset_file_csv, index=False)
 				dataset['relevant'] = dataset['relevant'].fillna(value=0)
 
 			# Iterate through the dataset
@@ -49,7 +54,6 @@ class Command(BaseCommand):
 				for model in models:
 					prediction = pipelines[model].predict([row['terms']])
 					new_predictions[f'{model}'] = prediction[0] == 1
-					print(new_predictions)
 				# Create a new MLPrediction object and save it
 				ml_prediction_instance = MLPredictions(
 					**new_predictions  # Unpack predictions dictionary to fields
@@ -58,7 +62,6 @@ class Command(BaseCommand):
 
 				# Link the new prediction to the article and save the article
 				article.ml_predictions.add(ml_prediction_instance)
-				print(article)
 				article.save()
 
 				print(f'Predictions saved for Article ID {article.pk}')
