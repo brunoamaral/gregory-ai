1d2974abce44e19cb44fdba19b51766135c16fc0
Bruno Amaral <mail@brunoamaral.eu>
2024-05-25 12:34:38 +0100

    use django ORM and update to use TeamCategories

--- django/gregory/management/commands/rebuild_categories.py
+++ django/gregory/management/commands/rebuild_categories.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
-import os
-import psycopg2
-from dotenv import load_dotenv
+from django.db.models import Q
+from gregory.models import Articles, Trials, TeamCategory, Articles, Trials
 
 class Command(BaseCommand):
 	help = 'Rebuilds category associations for articles and trials.'
@@ -12,57 +11,47 @@ class Command(BaseCommand):
 		self.stdout.write(self.style.SUCCESS('Successfully rebuilt category associations for articles and trials.'))
 
 	def rebuild_cats_articles(self):
-		load_dotenv()
-		# Get environment variables
-		db_host = os.getenv('DB_HOST')
-		postgres_user = os.getenv('POSTGRES_USER')
-		postgres_password = os.getenv('POSTGRES_PASSWORD')
-		postgres_db = os.getenv('POSTGRES_DB')
+		# Clear existing relationships
+		Articles.team_categories.through.objects.all().delete()
 
-		try:
-			conn = psycopg2.connect(dbname=postgres_db, user=postgres_user, host=db_host, password=postgres_password)
-		except Exception as e:
-			self.stdout.write(f"Unable to connect to the database: {e}")
-			return
-
-		cur = conn.cursor()
-		cur.execute("DELETE FROM articles_categories;")
-		cur.execute("SELECT category_name, category_terms, category_id FROM categories;")
-		categories = cur.fetchall()
+		# Get all team categories
+		categories = TeamCategory.objects.all()
 
 		for cat in categories:
-			terms = f"(%{'%|%'.join(cat[1]).lower()}%)"
-			cat_id = cat[2]
-			cur.execute("""INSERT INTO articles_categories (articles_id, categories_id)
-						   SELECT "articles"."article_id", "categories"."category_id" FROM "categories"
-						   INNER JOIN "articles" ON lower("articles"."title") SIMILAR TO %s AND "categories"."category_id" = %s
-						""", (terms, cat_id))
-			conn.commit()
+			terms = cat.category_terms
+			team_id = cat.team_id
+
+			# Build the Q object for filtering articles
+			query = Q()
+			for term in terms:
+				query |= Q(title__icontains=term)
+
+			# Filter articles based on the team and the terms
+			articles = Articles.objects.filter(query, teams__id=team_id)
+
+			# Associate articles with the team category
+			for article in articles:
+				article.team_categories.add(cat)
 
 	def rebuild_cats_trials(self):
-		load_dotenv()
-		# Repeat similar steps for trials_categories
-		db_host = os.getenv('DB_HOST')
-		postgres_user = os.getenv('POSTGRES_USER')
-		postgres_password = os.getenv('POSTGRES_PASSWORD')
-		postgres_db = os.getenv('POSTGRES_DB')
+		# Clear existing relationships
+		Trials.team_categories.through.objects.all().delete()
 
-		try:
-			conn = psycopg2.connect(dbname=postgres_db, user=postgres_user, host=db_host, password=postgres_password)
-		except Exception as e:
-			self.stdout.write(f"Unable to connect to the database: {e}")
-			return
-
-		cur = conn.cursor()
-		cur.execute("DELETE FROM trials_categories;")
-		cur.execute("SELECT category_name, category_terms, category_id FROM categories;")
-		categories = cur.fetchall()
+		# Get all team categories
+		categories = TeamCategory.objects.all()
 
 		for cat in categories:
-			terms = f"(%{'%|%'.join(cat[1]).lower()}%)"
-			cat_id = cat[2]
-			cur.execute("""INSERT INTO trials_categories (trials_id, categories_id)
-						   SELECT "trials"."trial_id", "categories"."category_id" FROM "categories"
-						   INNER JOIN "trials" ON lower("trials"."title") SIMILAR TO %s AND "categories"."category_id" = %s
-						""", (terms, cat_id))
-			conn.commit()
+			terms = cat.category_terms
+			team_id = cat.team_id
+
+			# Build the Q object for filtering trials
+			query = Q()
+			for term in terms:
+				query |= Q(title__icontains=term)
+
+			# Filter trials based on the team and the terms
+			trials = Trials.objects.filter(query, teams__id=team_id)
+
+			# Associate trials with the team category
+			for trial in trials:
+				trial.team_categories.add(cat)
