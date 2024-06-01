adc8586aae99609b6c82175b2a01996f70590701
Bruno Amaral <mail@brunoamaral.eu>
2024-06-01 18:11:42 +0100

    catch possible errors and continue with the pipeline

--- django/gregory/management/commands/pipeline.py
+++ django/gregory/management/commands/pipeline.py
@@ -6,24 +6,21 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		commands_to_run = [
-			# 1. Feedreader. Get articles and try to fetch some data
-			'feedreader', 
-			# 2. Find missing DOI
-			'find_doi',
-			# 3. Find missing data
-			'update_articles_info', 
-			# 4. Find missing authors
-			'get_authors', 
-			# 5. Find missing ORCID for authors
-			'update_orcid',
-			# 4. Assign categories
-			'rebuild_categories', 
-			# 5. Get takeaways
-			'get_takeaways',
-			# 6. Predict
-			'3_predict', ]
+			'feedreader',       		# 1. Feedreader. Get articles and try to fetch some data
+			'find_doi',         		# 2. Find missing DOI
+			'update_articles_info', # 3. Find missing data
+			'get_authors',      		# 4. Find missing authors
+			'update_orcid',     		# 5. Find missing ORCID for authors
+			'rebuild_categories',   # 6. Assign categories
+			'get_takeaways',    		# 7. Get takeaways
+			'3_predict',        		# 8. Predict
+		]
 
 		for cmd in commands_to_run:
-			self.stdout.write(self.style.SUCCESS(f'Running command: {cmd}'))
-			call_command(cmd)
-			self.stdout.write(self.style.SUCCESS(f'Finished command: {cmd}'))
+			try:
+				self.stdout.write(self.style.SUCCESS(f'Running command: {cmd}'))
+				call_command(cmd)
+				self.stdout.write(self.style.SUCCESS(f'Finished command: {cmd}'))
+			except Exception as e:
+				self.stderr.write(self.style.ERROR(f'Error running command {cmd}: {str(e)}'))
+				self.stdout.write(self.style.WARNING(f'Skipping command: {cmd}'))
