0cee4f6dd2c409489762fb58a22c6da55d0f9de6
Bruno Amaral <mail@brunoamaral.eu>
2024-09-15 13:34:55 +0100

    add debug output

--- django/gregory/management/commands/update_orcid.py
+++ django/gregory/management/commands/update_orcid.py
@@ -17,7 +17,12 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **kwargs):
 		orcid_api = orcid.PublicAPI(self.orcid_key, self.orcid_secret, sandbox=False)
-		token = orcid_api.get_search_token_from_orcid()
+		try:
+			token = orcid_api.get_search_token_from_orcid()
+		except requests.exceptions.HTTPError as e:
+			print(f"Failed to retrieve token from ORCID API: {e}")
+			return  # Stop execution if token retrieval fails
+
 		three_months_ago = timezone.now() - timezone.timedelta(days=90)
 		authors = Authors.objects.annotate(num_articles=Count('articles')).filter(
 						Q(orcid_check__lte=three_months_ago) | Q(orcid_check__isnull=True),
@@ -27,28 +32,27 @@ class Command(BaseCommand):
 
 		for author in authors:
 			try:
-				initial_country = author.country  # Capture the initial state
+				initial_country = author.country
 				author_orcid_number = author.ORCID.replace('http://orcid.org/', '')
 				record = orcid_api.read_record_public(author_orcid_number, 'record', token)
 				addresses = record.get('person', {}).get('addresses', {}).get('address', [])
 				orcid_check = timezone.now()
 				author.orcid_check = orcid_check
-				print(author)
-				if addresses:
-						country_code = addresses[0].get('country', {}).get('value')
-						author.country = country_code  # CountryField will handle the country code correctly
-						change_reason = 'Updated country from ORCID API.'
-				else:
-						print(f"No address found for author with ORCID: {author_orcid_number}")
-						change_reason = 'Attempted to update country from ORCID API but no address found.'
 
-				# Save the author object with history tracking
+				print(author)  # Debugging
+
+				if addresses:
+					country_code = addresses[0].get('country', {}).get('value')
+					author.country = country_code
+					change_reason = 'Updated country from ORCID API.'
+				else:
+					print(f"No address found for author with ORCID: {author_orcid_number}")
+					change_reason = 'Attempted to update country from ORCID API but no address found.'
+
 				author.save()
-				# Optionally, use update_change_reason to record why the change was made
-				if initial_country != author.country:  # Check if the country was actually updated
-						update_change_reason(author, change_reason)
+				if initial_country != author.country:
+					update_change_reason(author, change_reason)
+					
 			except requests.exceptions.HTTPError as e:
 				print(f"Failed to update author with ORCID: {author_orcid_number}. Error: {e}")
-				# Optionally, you can log the error to a file or a logging system here
-				continue  # This will skip the rest of the current iteration and move to the next author
-
+				continue
