17966c7d71e28585fed76e0454e7d47ef0a32435
Bruno Amaral <mail@brunoamaral.eu>
2024-12-21 14:09:57 +0000

    fixes errors with feedparser and tzinfo

--- django/gregory/management/commands/feedreader_trials.py
+++ django/gregory/management/commands/feedreader_trials.py
@@ -1,164 +1,137 @@
 from django.core.management.base import BaseCommand
-from gregory.models import Articles, Trials, Sources, Authors
-from crossref.restful import Works, Etiquette
-from dateutil.parser import parse
-from dateutil.tz import gettz
-from django.core.exceptions import MultipleObjectsReturned
 from django.db import IntegrityError
 from django.db.models import Q
 from django.utils import timezone
-from gregory.classes import SciencePaper, ClinicalTrial
-from sitesettings.models import CustomSetting
-import feedparser
-import gregory.functions as greg
-import os
+from gregory.models import Trials, Sources
+from gregory.classes import ClinicalTrial
+from gregory.functions import remove_utm
+from simple_history.utils import update_change_reason
+from dateutil.parser import parse
 import pytz
 import re
+import feedparser
 import requests
-from simple_history.utils import update_change_reason
 
-def update_trials_from_feeds(self):
-	sources = Sources.objects.filter(method='rss', source_for='trials')
-	for source in sources:
-		feed = self.fetch_feed(source.link, source.ignore_ssl)
-		for entry in feed['entries']:
-			summary = ''
-			if hasattr(entry,'summary_detail'):
-				summary = entry['summary_detail']['value']
-			if hasattr(entry,'summary'):
-				summary = entry['summary']
-			published = entry.get('published')
-			if published:
-				published = parse(entry['published'], tzinfos=self.tzinfos).astimezone(pytz.utc)
-			link = greg.remove_utm(entry['link'])
-			eudract = None
-			euct = None
-			nct = None
-			if "clinicaltrialsregister.eu" in link:
-				match = re.search(r'eudract_number\%3A(\d{4}-\d{6}-\d{2})', link)
-				if match:
-					eudract = match.group(1)
-					euct = match.group(1)
-			if 'clinicaltrials.gov' in link:
-				nct = entry['guid']
-			identifiers = {
-				"eudract": eudract if eudract is not None else None,
-				"euct": euct if euct is not None else None,
-				"nct": nct if nct is not None else None
-			}
-			clinical_trial = ClinicalTrial(title = entry['title'], summary = summary, link = link, published_date = published, identifiers = identifiers,)
-			clinical_trial.clean_summary()
-			# Find if there's already a trial with the same identifiers
-			print(f"trying to find {clinical_trial} in db...")
-			query = Q()
-			for key, value in identifiers.items():
-					if value:
-							query |= Q(**{f'identifiers__{key}': value})
+class Command(BaseCommand):
+	def handle(self, *args, **options):
+		self.setup()
+		self.update_trials_from_feeds()
 
-			# Use the dynamically constructed query to filter existing trials
-			existing_trial = Trials.objects.filter(query).first()
-			if not existing_trial:
-					print(f"Didn't find trial by identifier, trying title match...")
-					existing_trial = Trials.objects.filter(title=entry['title']).first()
-					if existing_trial:
-							print(f"Found existing trial by title: {existing_trial.pk}")
-					else:
-							print("No existing trial found by title.")
-			else:
-					print(f"Found existing trial by identifier: {existing_trial.pk}")          
-			if existing_trial:
-				# Capture the initial state of the trial
-				initial_state = {
-						'title': existing_trial.title,
-						'summary': existing_trial.summary,
-						'link': existing_trial.link,
-						'published_date': existing_trial.published_date,
-						'identifiers': existing_trial.identifiers,
-				}
-				try:
-						# Attempt to update the existing trial fields including the title
-						existing_trial.title = clinical_trial.title
-						existing_trial.summary = clinical_trial.summary
-						existing_trial.link = clinical_trial.link
-						existing_trial.published_date = clinical_trial.published_date
-						existing_trial.identifiers = clinical_trial.identifiers
-						existing_trial.source = source
-						existing_trial.sources.add(source)
-						existing_trial.teams.add(source.team)
-						existing_trial.subjects.add(source.subject)
-						existing_trial.save()
-				except IntegrityError:
-						# If an IntegrityError occurs, update all except the title
-						existing_trial.summary = clinical_trial.summary
-						existing_trial.link = clinical_trial.link
-						existing_trial.published_date = clinical_trial.published_date
-						existing_trial.identifiers = clinical_trial.identifiers
-						existing_trial.source = source
-						existing_trial.teams.add(source.team)
-						existing_trial.subjects.add(source.subject)
-						existing_trial.sources.add(source)
-						# Explicitly save only the fields that were updated to avoid the IntegrityError
-						existing_trial.save(update_fields=['summary', 'link', 'published_date', 'identifiers', 'source'])
-						print("Updated trial information, excluding the title due to IntegrityError.")
-				if any(initial_state[field] != getattr(existing_trial, field) for field in initial_state):
-					change_reason = "Updated from RSS feed."
-					update_change_reason(existing_trial, change_reason)
-					print(f"Trial {existing_trial.pk} updated.")
-				else:
-						print(f"No changes detected for Trial {existing_trial.pk}.")
+	def setup(self):
+		self.tzinfos = {"EDT": pytz.timezone("America/New_York"), "EST": pytz.timezone("America/New_York")}
+
+	def update_trials_from_feeds(self):
+		"""Fetch and process RSS feeds for clinical trials."""
+		sources = Sources.objects.filter(method='rss', source_for='trials')
+		for source in sources:
+			feed = self.fetch_feed(source.link, source.ignore_ssl)
+			for entry in feed['entries']:
+				clinical_trial = self.process_feed_entry(entry, source)
+				if clinical_trial:
+					self.sync_clinical_trial(clinical_trial, source)
+
+	def fetch_feed(self, link: str, ignore_ssl: bool):
+		"""Fetch the RSS feed for a given source."""
+		if not ignore_ssl:
+			return feedparser.parse(link)
 		else:
-			# Create a new trial
-			try:
-				q_objects = Q()
-				if clinical_trial.identifiers.get('nct'):
-					q_objects |= Q(identifiers__nct=clinical_trial.identifiers.get('nct'))
-				if clinical_trial.identifiers.get('eudract'):
-					q_objects |= Q(identifiers__eudract=clinical_trial.identifiers.get('eudract'))
-				if clinical_trial.identifiers.get('euct'):
-					q_objects |= Q(identifiers__euct=clinical_trial.identifiers.get('euct'))
-				trial = Trials.objects.get(q_objects)
-			except Trials.DoesNotExist:
-				# If the trial doesn't exist, create a new one
-				try:
-					print(f'trying to create {clinical_trial.identifiers}...')
-					trial = Trials.objects.create(
-						discovery_date=timezone.now(),
-						title=clinical_trial.title,
-						summary=clinical_trial.summary,
-						link=clinical_trial.link,
-						published_date=clinical_trial.published_date,
-						identifiers=clinical_trial.identifiers,
-					)
-					trial.teams.add(source.team)
-					trial.subjects.add(source.subject)
-					trial.sources.add(source)
-					print(f'created {trial.trial_id}?')
-				except IntegrityError as e:
-					print(f"An integrity error occurred: {str(e)}")				
-			except MultipleObjectsReturned as e:
-				print(f"Multiple entries were found for the same trial identifiers: {str(e)}")
-				duplicate_trials = Trials.objects.filter(
-					Q(identifiers__nct=clinical_trial.identifiers.get('nct')) |
-					Q(identifiers__eudract=clinical_trial.identifiers.get('eudract')) |
-					Q(identifiers__euct=clinical_trial.identifiers.get('euct'))
-				)
-				duplicate_ids = [trial.trial_id for trial in duplicate_trials]
-				print("Warning: multiple Trials entries found for identifier. The IDs of the duplicates are: ", duplicate_ids, ". Please resolve manually.")
+			response = requests.get(link, verify=False)
+			return feedparser.parse(response.content)
 
-			else:
-			# If the trial exists, update it
-				try:
-					trial = Trials.objects.get(pk=trial.pk)
-					trial.title = clinical_trial.title
-					trial.summary = clinical_trial.summary
-					trial.link = clinical_trial.link
-					trial.published_date = clinical_trial.published_date
-					trial.identifiers = clinical_trial.identifiers
-					trial.teams.add(source.team)
-					trial.subjects.add(source.subject)
-					trial.sources.add(source)
-					trial.save()						
-				except Exception as e:
-					print(f"An error occurred: {str(e)}")
-					pass
-			pass
+	def process_feed_entry(self, entry: dict, source) -> ClinicalTrial:
+		"""Process an RSS feed entry into a ClinicalTrial object."""
+		summary = entry.get('summary', entry.get('summary_detail', {}).get('value', ''))
+		published = self.parse_date(entry.get('published'))
+		link = remove_utm(entry['link'])
+
+		identifiers = self.extract_identifiers(link, entry.get('guid'))
+		return ClinicalTrial(
+			title=entry['title'],
+			summary=summary,
+			link=link,
+			published_date=published,
+			identifiers=identifiers,
+		)
+
+	def parse_date(self, date_str: str):
+		"""Parse a date string into a timezone-aware datetime."""
+		if not date_str:
+			return None
+		return parse(date_str, tzinfos=self.tzinfos).astimezone(pytz.utc)
+
+	def extract_identifiers(self, link: str, guid: str) -> dict:
+		"""Extract identifiers (e.g., NCT, EudraCT) from the link or GUID."""
+		eudract = re.search(r'eudract_number%3A(\d{4}-\d{6}-\d{2})', link)
+		nct = guid if 'clinicaltrials.gov' in link else None
+		return {
+			"eudract": eudract.group(1) if eudract else None,
+			"nct": nct,
+			"euct": eudract.group(1) if eudract else None,
+		}
+
+	def sync_clinical_trial(self, clinical_trial: ClinicalTrial, source):
+		"""Sync a ClinicalTrial object with the database."""
+		existing_trial = self.find_existing_trial(clinical_trial)
+
+		if existing_trial:
+			self.update_existing_trial(existing_trial, clinical_trial, source)
+		else:
+			self.create_new_trial(clinical_trial, source)
+
+	def find_existing_trial(self, clinical_trial: ClinicalTrial):
+		"""Find an existing trial by identifiers or title."""
+		query = Q()
+		for key, value in clinical_trial.identifiers.items():
+			if value:
+				query |= Q(**{f'identifiers__{key}': value})
+		return Trials.objects.filter(query).first() or Trials.objects.filter(title=clinical_trial.title).first()
+
+	def update_existing_trial(self, existing_trial, clinical_trial, source):
+		"""Update an existing trial."""
+		initial_state = {field: getattr(existing_trial, field) for field in ['title', 'summary', 'link', 'published_date', 'identifiers']}
+		try:
+			self.apply_trial_updates(existing_trial, clinical_trial, source)
+		except IntegrityError:
+			self.apply_trial_updates(existing_trial, clinical_trial, source, exclude_title=True)
+		
+		if any(initial_state[field] != getattr(existing_trial, field) for field in initial_state):
+			update_change_reason(existing_trial, "Updated from RSS feed.")
+			existing_trial.save()
+			print(f"Trial {existing_trial.pk} updated.")
+		else:
+			print(f"No changes detected for Trial {existing_trial.pk}.")
+
+	def apply_trial_updates(self, trial, clinical_trial, source, exclude_title=False):
+		"""Apply updates to a trial."""
+		if not exclude_title:
+			trial.title = clinical_trial.title
+		trial.summary = clinical_trial.summary
+		trial.link = clinical_trial.link
+		trial.published_date = clinical_trial.published_date
+		trial.identifiers = clinical_trial.identifiers
+		trial.save(update_fields=['summary', 'link', 'published_date', 'identifiers'])
+
+		# Update Many-to-Many relationships
+		trial.sources.add(source)
+		trial.teams.add(source.team)
+		trial.subjects.add(source.subject)
+
+	def create_new_trial(self, clinical_trial: ClinicalTrial, source):
+		"""Create a new trial in the database."""
+		try:
+			trial = Trials.objects.create(
+				discovery_date=timezone.now(),
+				title=clinical_trial.title,
+				summary=clinical_trial.summary,
+				link=clinical_trial.link,
+				published_date=clinical_trial.published_date,
+				identifiers=clinical_trial.identifiers,
+			)
+			# Save first, then update Many-to-Many relationships
+			trial.save()
+			trial.sources.add(source)
+			trial.teams.add(source.team)
+			trial.subjects.add(source.subject)
+			print(f"Created new trial: {trial.trial_id}")
+		except IntegrityError as e:
+			print(f"Integrity error during trial creation: {e}")
