e7ce6af5824e674cfa60e7590b0e97381e25281d
Bruno Amaral <mail@brunoamaral.eu>
2024-12-24 18:15:06 +0000

    changes the way to get website_domain

--- django/rss/views.py
+++ django/rss/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.syndication.views import Feed
-from django.conf import settings
+from django.contrib.sites.models import Site
 from gregory.models import Articles, Authors, Trials, Sources, TeamCategory
 from django.urls import reverse
+from sitesettings.models import CustomSetting
 
+# Helper function to get the website domain dynamically
+def get_website_domain():
+	current_site = Site.objects.get_current()
+	custom_setting = CustomSetting.objects.filter(site=current_site).first()
+	return custom_setting.admin_email if custom_setting else current_site.domain
 
 
 class LatestArticlesFeed(Feed):
 	title = "Latest research articles"
 	link = "/articles/"
-	description = "Real time results for research on Multiple Sclerosis."
+	description = "Real-time results for research on Multiple Sclerosis."
 
 	def items(self):
 		return Articles.objects.order_by('-discovery_date')[:5]
@@ -22,25 +26,21 @@ class LatestArticlesFeed(Feed):
 	def item_description(self, item):
 		return item.summary
 
-	# # item_link is only needed if NewsItem has no get_absolute_url method.
 	def item_link(self, item):
-		return 'https://'+ settings.WEBSITE_DOMAIN + '/articles/' + str(item.pk) + '/' 
+		return f"https://{get_website_domain()}/articles/{str(item.pk)}/"
 
 	def item_pubdate(self, item):
-		"""
-		Takes an item, as returned by items(), and returns the item's
-		pubdate.
-		"""
 		return item.published_date
+
 
 class ArticlesBySubjectFeed(Feed):
 	title = "Latest research articles by Subject"
 	link = "/articles/"
-	description = "Real time results for research on Multiple Sclerosis."
+	description = "Real-time results for research on Multiple Sclerosis."
+
 	def get_object(self, request, subject):
-			subject = subject.replace('-', ' ')
-			subject = Sources.objects.filter(subject__iregex=subject)
-			return subject
+		subject = subject.replace('-', ' ')
+		return Sources.objects.filter(subject__iregex=subject)
 
 	def items(self, subject):
 		return Articles.objects.filter(source__in=subject).order_by('-discovery_date')[:5]
@@ -51,16 +51,12 @@ class ArticlesBySubjectFeed(Feed):
 	def item_description(self, item):
 		return item.summary
 
-	# # item_link is only needed if NewsItem has no get_absolute_url method.
 	def item_link(self, item):
-		return 'https://'+ settings.WEBSITE_DOMAIN + '/articles/' + str(item.pk) + '/' 
+		return f"https://{get_website_domain()}/articles/{str(item.pk)}/"
 
 	def item_pubdate(self, item):
-		"""
-		Takes an item, as returned by items(), and returns the item's
-		pubdate.
-		"""
 		return item.published_date
+
 
 class ArticlesByCategoryFeed(Feed):
 	title = "Latest research articles by Subject"
@@ -68,14 +64,12 @@ class ArticlesByCategoryFeed(Feed):
 	description = "Real-time results for research on Multiple Sclerosis."
 
 	def get_object(self, request, team_id, category_slug):
-		# Retrieve the team category based on team_id and category_slug
 		category = TeamCategory.objects.filter(team__id=team_id, category_slug=category_slug).first()
 		if not category:
 			raise Http404("Category does not exist")
 		return category
 
 	def items(self, obj):
-		# Return the latest articles for the given category
 		return Articles.objects.filter(team_categories=obj).order_by('-discovery_date')[:10]
 
 	def item_title(self, item):
@@ -85,20 +79,16 @@ class ArticlesByCategoryFeed(Feed):
 		return item.summary
 
 	def item_link(self, item):
-		# item_link is only needed if NewsItem has no get_absolute_url method.
-		return 'https://' + settings.WEBSITE_DOMAIN + '/articles/' + str(item.pk) + '/'
+		return f"https://{get_website_domain()}/articles/{str(item.pk)}/"
 
 	def item_pubdate(self, item):
-		"""
-		Takes an item, as returned by items(), and returns the item's
-		pubdate.
-		"""
 		return item.published_date
+
 
 class LatestTrialsFeed(Feed):
 	title = "Latest clinical trials"
 	link = "/trials/"
-	description = "Real time results for research on Multiple Sclerosis."
+	description = "Real-time results for research on Multiple Sclerosis."
 
 	def items(self):
 		return Trials.objects.order_by('-discovery_date')[:5]
@@ -109,65 +99,52 @@ class LatestTrialsFeed(Feed):
 	def item_description(self, item):
 		return item.summary
 
-	# # item_link is only needed if NewsItem has no get_absolute_url method.
 	def item_link(self, item):
-		return 'https://api.' + settings.WEBSITE_DOMAIN + '/trials/' + str(item.pk) + '/'
+		return f"https://{get_website_domain()}/trials/{str(item.pk)}/"
 
 	def item_pubdate(self, item):
-		"""
-		Takes an item, as returned by items(), and returns the item's
-		pubdate.
-		"""
 		return item.published_date
+
 
 class MachineLearningFeed(Feed):
 	title = "Relevant articles by machine learning"
 	link = "/articles/"
-	description = "Real time results for research on Multiple Sclerosis."
+	description = "Real-time results for research on Multiple Sclerosis."
 
 	def items(self):
 		return Articles.objects.filter(ml_prediction_gnb=True).order_by('-discovery_date')[:20]
-	
+
 	def item_title(self, item):
 		return item.title
 
 	def item_description(self, item):
 		return item.link
 
+	def item_link(self, item):
+		return f"https://{get_website_domain()}/articles/{str(item.pk)}/"
+
 	def item_pubdate(self, item):
-		"""
-		Takes an item, as returned by items(), and returns the item's
-		pubdate.
-		"""
 		return item.published_date
 
-	# # item_link is only needed if NewsItem has no get_absolute_url method.
-	def item_link(self, item):
-		return 'https://'+ settings.WEBSITE_DOMAIN + '/articles/' + str(item.pk) + '/' 
 
 class ToPredictFeed(Feed):
 	title = "Relevant articles by machine learning"
 	link = "/articles/"
-	description = "Real time prediction results"
+	description = "Real-time prediction results."
 
 	def items(self):
 		return Articles.objects.filter(ml_prediction_gnb=None)[:20]
-	
+
 	def item_title(self, item):
 		return item.title
 
 	def item_description(self, item):
 		return item.summary
 
-	# # item_link is only needed if NewsItem has no get_absolute_url method.
 	def item_link(self, item):
-		return 'https://'+ settings.WEBSITE_DOMAIN + '/articles/' + str(item.pk) + '/' 
+		return f"https://{get_website_domain()}/articles/{str(item.pk)}/"
 
 	def item_pubdate(self, item):
-		"""
-		Takes an item, as returned by items(), and returns the item's
-		pubdate.
-		"""
 		return item.published_date
 
 
@@ -178,39 +155,26 @@ class OpenAccessFeed(Feed):
 
 	def items(self):
 		return Articles.objects.filter(access='open').order_by('-discovery_date')[:20]
-	
+
 	def item_title(self, item):
 		return item.title
 
 	def item_description(self, item):
 		return item.summary
 
+	def item_link(self, item):
+		return f"https://{get_website_domain()}/articles/{str(item.pk)}/"
+
 	def item_pubdate(self, item):
-		"""
-		Takes an item, as returned by items(), and returns the item's
-		pubdate.
-		"""
 		return item.published_date
 
-	# # item_link is only needed if NewsItem has no get_absolute_url method.
-	def item_link(self, item):
-		return item.link
-	
 
 class ArticlesByAuthorFeed(Feed):
 	title = "Articles by Author"
 	link = "/feed/articles/"
-	description = "RSS feed for articles by a specific author. Use `/feed/articles/author/<author_id>/`"
+	description = "RSS feed for articles by a specific author."
 
-	def __init__(self, **kwargs):
-		self.author_id = kwargs.get('author_id')
-		super().__init__(**kwargs)
-
-	def get(self, request, *args, **kwargs):
-		self.author_id = kwargs.get('author_id')
-		return super().get(request, *args, **kwargs)
-
-	def get_object(self, request, author_id, *args, **kwargs):
+	def get_object(self, request, author_id):
 		return Authors.objects.get(pk=author_id)
 
 	def items(self, obj):
@@ -223,19 +187,7 @@ class ArticlesByAuthorFeed(Feed):
 		return item.summary
 
 	def item_link(self, item):
-		return item.link
-
-	def item_guid(self, item):
-		return item.link
+		return f"https://{get_website_domain()}/articles/{str(item.pk)}/"
 
 	def item_pubdate(self, item):
 		return item.published_date
-
-	def item_author_name(self, item):
-		return ', '.join([author.full_name for author in item.authors.all()])
-
-	def item_categories(self, item):
-		return [category.category_name for category in item.categories.all()]
-
-	def link(self, obj):
-		return reverse('articles_by_author_feed', kwargs={'author_id': obj.pk})
