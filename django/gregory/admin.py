ccc0f68f9839411936d5f387456c4887418d20c8
Bruno Amaral <mail@brunoamaral.eu>
2024-12-29 13:55:21 +0000

    converted spaces into tabs for consistency

--- django/gregory/admin.py
+++ django/gregory/admin.py
@@ -9,109 +9,109 @@ from django.utils.html import format_html
 from django.urls import reverse
 
 class ArticleSubjectRelevanceInline(admin.TabularInline):
-    model = ArticleSubjectRelevance
-    extra = 1
+	model = ArticleSubjectRelevance
+	extra = 1
 
 class ArticleAdminForm(forms.ModelForm):
-    ml_predictions_display = MLPredictionsField(required=False)
+	ml_predictions_display = MLPredictionsField(required=False)
 
-    class Meta:
-        model = Articles
-        fields = '__all__'
-        widgets = {
-            'ml_predictions': MLPredictionsWidget(),
-        }
+	class Meta:
+		model = Articles
+		fields = '__all__'
+		widgets = {
+			'ml_predictions': MLPredictionsWidget(),
+		}
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance and self.instance.pk:
-            self.fields['ml_predictions_display'].initial = self.instance.ml_predictions.all()
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		if self.instance and self.instance.pk:
+			self.fields['ml_predictions_display'].initial = self.instance.ml_predictions.all()
 
 class ArticleAdmin(SimpleHistoryAdmin):
-    form = ArticleAdminForm
-    inlines = [ArticleSubjectRelevanceInline]
-    fieldsets = (
-        ('Article Information', {
-            'fields': (
-                'title', 'link', 'doi', 'summary', 'teams', 'subjects', 'sources',
-                'published_date', 'discovery_date', 'authors', 'team_categories',
-                'entities', 'kind', 'access',
-                'publisher', 'container_title', 'crossref_check', 'takeaways',
-            ),
-            'description': 'This section contains general information about the article'
-        }),
-        ('Machine Learning Relevancy Predictions per Subject', {
-            'fields': ('ml_predictions_display',),
-            'description': 'Grouping machine learning prediction indicators',
-        }),
-    )
-    list_display = ['article_id', 'title']
-    ordering = ['-discovery_date']
-    readonly_fields = ['entities', 'discovery_date']
-    search_fields = ['article_id', 'title', 'doi']
-    list_filter = ('subjects',)
-    raw_id_fields = ("authors",)
+	form = ArticleAdminForm
+	inlines = [ArticleSubjectRelevanceInline]
+	fieldsets = (
+		('Article Information', {
+			'fields': (
+				'title', 'link', 'doi', 'summary', 'teams', 'subjects', 'sources',
+				'published_date', 'discovery_date', 'authors', 'team_categories',
+				'entities', 'kind', 'access',
+				'publisher', 'container_title', 'crossref_check', 'takeaways',
+			),
+			'description': 'This section contains general information about the article'
+		}),
+		('Machine Learning Relevancy Predictions per Subject', {
+			'fields': ('ml_predictions_display',),
+			'description': 'Grouping machine learning prediction indicators',
+		}),
+	)
+	list_display = ['article_id', 'title']
+	ordering = ['-discovery_date']
+	readonly_fields = ['entities', 'discovery_date']
+	search_fields = ['article_id', 'title', 'doi']
+	list_filter = ('subjects',)
+	raw_id_fields = ("authors",)
 
 class TrialAdmin(SimpleHistoryAdmin):
-    list_display = ['trial_id', 'title', 'discovery_date', 'last_updated']
-    exclude = ['ml_predictions','relevant']
-    readonly_fields = ['last_updated', 'team_categories']
-    search_fields = ['trial_id', 'title']
-    list_filter = ['teams', 'subjects', 'sources']
+	list_display = ['trial_id', 'title', 'discovery_date', 'last_updated']
+	exclude = ['ml_predictions','relevant']
+	readonly_fields = ['last_updated', 'team_categories']
+	search_fields = ['trial_id', 'title']
+	list_filter = ['teams', 'subjects', 'sources']
 
 class SourceInline(admin.StackedInline):
-    model = Sources
-    extra = 1
+	model = Sources
+	extra = 1
 
 class SourceAdmin(admin.ModelAdmin):
-    list_display = ['name', 'source_for', 'subject', 'method']
-    list_filter = ['source_for', 'team', 'subject']
+	list_display = ['name', 'source_for', 'subject', 'method']
+	list_filter = ['source_for', 'team', 'subject']
 
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ['subject_name','description', 'view_sources','team']  # Display in the list view
-    readonly_fields = ['linked_sources']  # Display in the edit form
-    list_filter = ['team']  # Add the team filter
+	list_display = ['subject_name','description', 'view_sources','team']  # Display in the list view
+	readonly_fields = ['linked_sources']  # Display in the edit form
+	list_filter = ['team']  # Add the team filter
 
-    def view_sources(self, obj):
-        """Display sources as clickable links in the list view."""
-        sources = obj.sources_set.all()
-        if sources.exists():
-            links = [
-                format_html(
-                    '<a href="{}">{}</a>',
-                    reverse('admin:gregory_sources_change', args=[source.source_id]),
-                    source.name
-                )
-                for source in sources
-            ]
-            return format_html('<br>'.join(links))
-        return "No sources"
+	def view_sources(self, obj):
+		"""Display sources as clickable links in the list view."""
+		sources = obj.sources_set.all()
+		if sources.exists():
+			links = [
+				format_html(
+					'<a href="{}">{}</a>',
+					reverse('admin:gregory_sources_change', args=[source.source_id]),
+					source.name
+				)
+				for source in sources
+			]
+			return format_html('<br>'.join(links))
+		return "No sources"
 
-    view_sources.short_description = "Linked Sources"
+	view_sources.short_description = "Linked Sources"
 
-    def linked_sources(self, obj):
-        """Display sources as clickable links in the form view."""
-        sources = obj.sources_set.all()
-        if sources.exists():
-            links = [
-                format_html(
-                    '<a href="{}">{}</a>',
-                    reverse('admin:gregory_sources_change', args=[source.source_id]),
-                    source.name
-                )
-                for source in sources
-            ]
-            return format_html('<br>'.join(links))
-        return "No sources"
+	def linked_sources(self, obj):
+		"""Display sources as clickable links in the form view."""
+		sources = obj.sources_set.all()
+		if sources.exists():
+			links = [
+				format_html(
+					'<a href="{}">{}</a>',
+					reverse('admin:gregory_sources_change', args=[source.source_id]),
+					source.name
+				)
+				for source in sources
+			]
+			return format_html('<br>'.join(links))
+		return "No sources"
 
-    linked_sources.short_description = "Linked Sources"
+	linked_sources.short_description = "Linked Sources"
 class AuthorsAdmin(admin.ModelAdmin):
-    search_fields = ['family_name', 'given_name']
+	search_fields = ['family_name', 'given_name']
 
 @admin.register(TeamCategory)
 class TeamCategoryAdmin(admin.ModelAdmin):
-    list_display = ('team', 'category_name', 'category_slug')
-    search_fields = ('category_name', 'team__name')
+	list_display = ('team', 'category_name', 'category_slug')
+	search_fields = ('category_name', 'team__name')
 
 
 
