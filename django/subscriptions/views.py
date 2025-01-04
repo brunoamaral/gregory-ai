88c4453aef3201239b0013f02bbbf54d8532a9b9
Bruno Amaral <mail@brunoamaral.eu>
2025-01-04 15:01:25 +0000

    add get_or_create subscriber + update info

--- django/subscriptions/views.py
+++ django/subscriptions/views.py
@@ -9,24 +9,49 @@ import logging
 def subscribe_view(request):
 	logging.basicConfig(level=logging.INFO)
 	logger = logging.getLogger('__name__')
-	# Some processing if needed
+	
+	# Process the form data
 	subscriber_form = SubscribersForm(request.POST)
-	# check whether it's valid:
+	
 	if subscriber_form.is_valid():
 		first_name = subscriber_form.cleaned_data['first_name']
 		last_name = subscriber_form.cleaned_data['last_name']
 		email = subscriber_form.cleaned_data['email']
 		profile = subscriber_form.cleaned_data['profile']
-		list = subscriber_form.cleaned_data['list']
-		subscriber, created = Subscribers.objects.get_or_create( email=email, first_name=first_name, last_name=last_name)
-		subscriber.profile = profile
-		subscriber.subscriptions.add(list)
-		subscriber.save()
-		return HttpResponseRedirect('https://gregory-ms.com/thank-you/')
+		list_id = subscriber_form.cleaned_data['list']
+		
+		try:
+			# Fetch the list object
+			subscription_list = Lists.objects.get(pk=list_id)
+
+			# Check if the subscriber already exists
+			subscriber, created = Subscribers.objects.get_or_create(
+				email=email,
+				defaults={
+					'first_name': first_name,
+					'last_name': last_name,
+					'profile': profile
+				}
+			)
+
+			# If the subscriber already exists, update their details and add the list
+			if not created:
+				subscriber.first_name = first_name
+				subscriber.last_name = last_name
+				subscriber.profile = profile
+
+			# Add the list to the subscriber's subscriptions
+			subscriber.subscriptions.add(subscription_list)
+			subscriber.save()
+
+			return HttpResponseRedirect('https://gregory-ms.com/thank-you/')
+
+		except Lists.DoesNotExist:
+			logger.error(f"List with ID {list_id} does not exist.")
+			return HttpResponseRedirect('https://gregory-ms.com/error/')
+
 	else:
-		logger.error("Django log...")
-		logger.error(subscriber_form.is_valid())
+		# Log errors for debugging
+		logger.error("Form is invalid.")
 		logger.error(subscriber_form.errors)
-		logger.error(subscriber_form.cleaned_data['list'])
-		logger.error(subscriber_form.cleaned_data)
 		return HttpResponseRedirect('https://gregory-ms.com/error/')
