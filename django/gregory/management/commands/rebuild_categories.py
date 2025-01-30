3e0c673bf7e3c53415b9d36c42c2dce11109bb96
Bruno Amaral <mail@brunoamaral.eu>
2025-01-30 15:10:17 +0000

    also check trials summary for term

--- django/gregory/management/commands/rebuild_categories.py
+++ django/gregory/management/commands/rebuild_categories.py
@@ -55,7 +55,7 @@ class Command(BaseCommand):
 				# Build the Q object for filtering trials
 				query = Q()
 				for term in terms:
-					query |= Q(title__icontains=term)
+					query |= Q(title__icontains=term) | Q(summary__icontains=term)
 
 				# Filter trials based on the subject and terms
 				trials = Trials.objects.filter(query, subjects__id=subject_id)
