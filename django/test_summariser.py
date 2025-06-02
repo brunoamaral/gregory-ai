7e2ac895314128b65cac4037e35bfbec1ae0d2a3
Bruno Amaral <mail@brunoamaral.eu>
2025-06-02 21:33:29 +0100

    remove tests

--- django/test_summariser.py
+++
@@ -1,9 +0,0 @@
-
-from gregory.utils.summariser import summarise_bulk
-
-texts = ["Test text 1", "Test text 2", ""]
-print("Testing summarise_bulk function...")
-print(f"Input: {texts}")
-print(f"Output: {summarise_bulk(texts, batch_size=2)}")
-print("Test completed!")
-
