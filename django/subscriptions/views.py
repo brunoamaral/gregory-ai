e70b4672c5a42c2b617fed0507c3ab1258796d96
Bruno Amaral <mail@brunoamaral.eu>
2025-06-09 21:03:35 +0100

    fix indentation error

--- django/subscriptions/views.py
+++ django/subscriptions/views.py
@@ -7,54 +7,54 @@ import logging
 
 @csrf_exempt
 def subscribe_view(request):
-	logging.basicConfig(level=logging.INFO)
-	logger = logging.getLogger('__name__')
-	
-	# Process the form data
-        subscriber_form = SubscribersForm(request.POST)
-	
-	if subscriber_form.is_valid():
-		first_name = subscriber_form.cleaned_data['first_name']
-		last_name = subscriber_form.cleaned_data['last_name']
-		email = subscriber_form.cleaned_data['email']
-		profile = subscriber_form.cleaned_data['profile']
-                # ``request.POST`` may contain multiple ``list`` values when the
-                # user checks more than one subscription option. ``getlist``
-                # returns all of them as a list of strings.
-                list_ids = request.POST.getlist('list')
+		logging.basicConfig(level=logging.INFO)
+		logger = logging.getLogger('__name__')
 		
-                try:
-                        # Fetch all subscription lists selected by the user
-                        subscription_lists = Lists.objects.filter(pk__in=list_ids)
+		# Process the form data
+		subscriber_form = SubscribersForm(request.POST)
+		
+		if subscriber_form.is_valid():
+				first_name = subscriber_form.cleaned_data['first_name']
+				last_name = subscriber_form.cleaned_data['last_name']
+				email = subscriber_form.cleaned_data['email']
+				profile = subscriber_form.cleaned_data['profile']
+				# ``request.POST`` may contain multiple ``list`` values when the
+				# user checks more than one subscription option. ``getlist``
+				# returns all of them as a list of strings.
+				list_ids = request.POST.getlist('list')
 
-			# Check if the subscriber already exists
-			subscriber, created = Subscribers.objects.get_or_create(
-				email=email,
-				defaults={
-					'first_name': first_name,
-					'last_name': last_name,
-					'profile': profile
-				}
-			)
+				try:
+						# Fetch all subscription lists selected by the user
+						subscription_lists = Lists.objects.filter(pk__in=list_ids)
 
-			# If the subscriber already exists, update their details and add the list
-			if not created:
-				subscriber.first_name = first_name
-				subscriber.last_name = last_name
-				subscriber.profile = profile
+						# Check if the subscriber already exists
+						subscriber, created = Subscribers.objects.get_or_create(
+								email=email,
+								defaults={
+										'first_name': first_name,
+										'last_name': last_name,
+										'profile': profile
+								}
+						)
 
-                        # Add the lists to the subscriber's subscriptions
-                        subscriber.subscriptions.add(*subscription_lists)
-			subscriber.save()
+						# If the subscriber already exists, update their details and add the list
+						if not created:
+								subscriber.first_name = first_name
+								subscriber.last_name = last_name
+								subscriber.profile = profile
 
-			return HttpResponseRedirect('https://gregory-ms.com/thank-you/')
+						# Add the lists to the subscriber's subscriptions
+						subscriber.subscriptions.add(*subscription_lists)
+						subscriber.save()
 
-                except Exception as e:
-                        logger.error(f"Subscription error: {e}")
-                        return HttpResponseRedirect('https://gregory-ms.com/error/')
+						return HttpResponseRedirect('https://gregory-ms.com/thank-you/')
 
-	else:
-		# Log errors for debugging
-		logger.error("Form is invalid.")
-		logger.error(subscriber_form.errors)
-		return HttpResponseRedirect('https://gregory-ms.com/error/')
+				except Exception as e:
+						logger.error(f"Subscription error: {e}")
+						return HttpResponseRedirect('https://gregory-ms.com/error/')
+
+		else:
+				# Log errors for debugging
+				logger.error("Form is invalid.")
+				logger.error(subscriber_form.errors)
+				return HttpResponseRedirect('https://gregory-ms.com/error/')
