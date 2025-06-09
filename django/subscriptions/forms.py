7f1dd9f2fb0fbb29f6efe8fd5acb2ee988de9bbb
Bruno Amaral <mail@brunoamaral.eu>
2025-06-09 21:05:40 +0100

    fix indentation

--- django/subscriptions/forms.py
+++ django/subscriptions/forms.py
@@ -4,13 +4,13 @@ from django.forms import ModelForm
 from .models import Subscribers
 
 class SubscribersForm(ModelForm):
-        first_name = forms.CharField(max_length=100)
-        last_name = forms.CharField(max_length=100)
-        email = forms.EmailField(max_length=120)
-        # ``list`` was previously an ``IntegerField`` which only captured a
-        # single checkbox value.  It has been removed so the view can obtain all
-        # selected list IDs using ``request.POST.getlist('list')``.
-        class Meta:
+	first_name = forms.CharField(max_length=100)
+	last_name = forms.CharField(max_length=100)
+	email = forms.EmailField(max_length=120)
+	# ``list`` was previously an ``IntegerField`` which only captured a
+	# single checkbox value.  It has been removed so the view can obtain all
+	# selected list IDs using ``request.POST.getlist('list')``.
+	class Meta:
 		model = Subscribers
 		fields = [
 			'first_name',
