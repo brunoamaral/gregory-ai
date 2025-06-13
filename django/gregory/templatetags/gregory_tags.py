0d3cf4c9863becaeff5ac3c328ae0a6629202268
Bruno Amaral <mail@brunoamaral.eu>
2025-06-13 13:16:03 +0100

    implements a template filter to remove tags from strings

--- django/gregory/templatetags/gregory_tags.py
+++ django/gregory/templatetags/gregory_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from gregory.utils.text_utils import cleanHTML
+from bs4 import BeautifulSoup
+import re
 
 register = template.Library()
 
@@ -20,3 +23,31 @@ def split(value, delimiter):
     if value is None:
         return []
     return str(value).split(delimiter)
+
+@register.filter
+def clean_html_tags(value):
+    """
+    Clean HTML and XML tags from text for email display.
+    Removes tags like <div>, <jats:title>, <jats:sec>, etc.
+    Usage: {{ article.summary|clean_html_tags|truncatechars:300 }}
+    """
+    if not value:
+        return ""
+    
+    # Use BeautifulSoup to remove HTML/XML tags with proper spacing
+    soup = BeautifulSoup(value, 'html.parser')
+    cleaned = soup.get_text(separator=' ')
+    
+    # Additional regex cleanup for any remaining XML tags that might be missed
+    # Remove JATS tags like <jats:title>, <jats:sec>, etc.
+    cleaned = re.sub(r'<[^>]*jats:[^>]*>', ' ', cleaned)
+    cleaned = re.sub(r'</[^>]*jats:[^>]*>', ' ', cleaned)
+    
+    # Remove any remaining XML/HTML tags
+    cleaned = re.sub(r'<[^>]+>', ' ', cleaned)
+    
+    # Clean up extra whitespace
+    cleaned = re.sub(r'\s+', ' ', cleaned)
+    cleaned = cleaned.strip()
+    
+    return cleaned
