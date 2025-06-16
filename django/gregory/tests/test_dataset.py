29f5dad2296f6e7feebdbe22ddabb2bc9b077bed
Bruno Amaral <mail@brunoamaral.eu>
2025-06-16 23:20:09 +0100

    fix: resolve null value error in Team model tests

--- django/gregory/tests/test_dataset.py
+++ django/gregory/tests/test_dataset.py
@@ -13,6 +13,7 @@ from django.test import TestCase
 
 from gregory.utils.dataset import collect_articles, build_dataset, train_val_test_split
 from gregory.models import Team, Subject, Articles, ArticleSubjectRelevance
+from organizations.models import Organization  # Import Organization model
 
 
 class DatasetTestCase(TestCase):
@@ -20,9 +21,14 @@ class DatasetTestCase(TestCase):
     
     def setUp(self):
         """Set up test data."""
+        # Create an organization first
+        self.organization = Organization.objects.create(name='Test Organization')
+        
         # Create a team and subject
         self.team = Team.objects.create(
-            slug='test-team'
+            slug='test-team',
+            name='Test Team',
+            organization=self.organization
         )
         
         self.subject = Subject.objects.create(
@@ -57,6 +63,18 @@ class DatasetTestCase(TestCase):
         
     def test_collect_articles_with_window(self):
         """Test that windowed collection returns the correct subset."""
+        # Force the discovery dates to be further apart to ensure proper filtering
+        # Set the first 3 articles to be within the last 2 days
+        for i in range(3):
+            self.articles[i].discovery_date = datetime.now() - timedelta(hours=i)
+            self.articles[i].save()
+        
+        # Set the remaining articles to be older than 2 days
+        for i in range(3, 5):
+            self.articles[i].discovery_date = datetime.now() - timedelta(days=3, hours=i)
+            self.articles[i].save()
+            
+        # Now test the window filtering
         articles = collect_articles('test-team', 'test-subject', window_days=2)
         self.assertEqual(articles.count(), 3)  # Only articles from last 2 days
     
