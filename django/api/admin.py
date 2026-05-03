4f916461610f959007f4c4c461bb83cec8bb6184
Bruno Amaral <mail@brunoamaral.eu>
2026-05-03 19:19:20 +0100

    feat: add organization field to APIAccessScheme and update admin interface

--- django/api/admin.py
+++ django/api/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import APIAccessScheme, APIAccessSchemeLog
 
 class APIAcccessSchemeAdmin(admin.ModelAdmin):
-	list_display = ['api_key','client_name','client_contacts','ip_addresses','begin_date','end_date','max_calls_minute','max_calls_hour','max_calls_day']
+	list_display = ['api_key', 'client_name', 'client_contacts', 'organization', 'ip_addresses', 'begin_date', 'end_date', 'max_calls_minute', 'max_calls_hour', 'max_calls_day']
+	list_filter = ('organization',)
+	fields = ('api_key', 'client_name', 'client_contacts', 'organization', 'ip_addresses', 'begin_date', 'end_date', 'max_calls_minute', 'max_calls_hour', 'max_calls_day')
 	# a list of displayed columns name.
 
 class APILogAdmin(admin.ModelAdmin):
